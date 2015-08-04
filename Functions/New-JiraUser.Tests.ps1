ba3490ea8ea8eb0984362aa156dbd3d843a798b3
Joshua T <joshua@sonofone.net>
2015-08-04 17:13:15 -0500

    Created function New-JiraUser

---
+++ Functions/New-JiraUser.Tests.ps1
@@ -0,0 +1,78 @@
+﻿$here = Split-Path -Parent $MyInvocation.MyCommand.Path
+$sut = (Split-Path -Leaf $MyInvocation.MyCommand.Path).Replace(".Tests.", ".")
+. "$here\$sut"
+
+InModuleScope PSJira {
+    
+    # This is intended to be a parameter to the test, but Pester currently does not allow parameters to be passed to InModuleScope blocks.
+    # For the time being, we'll need to hard-code this and adjust it as desired.
+    $ShowMockData = $false
+    $ShowDebugData = $false
+
+    $jiraServer = 'http://jiraserver.example.com'
+
+    $testUsername = 'powershell-test'
+    $testEmail = "$testUsername@example.com"
+    $testDisplayName = 'Test User'
+
+    # Trimmed from this example JSON: expand, groups, avatarURL
+    $testJson = @"
+{
+    "self": "$jiraServer/rest/api/2/user?username=testUser",
+    "key": "$testUsername",
+    "name": "$testUsername",
+    "emailAddress": "$testEmail",
+    "displayName": "$testDisplayName",
+    "active": true
+}
+"@
+
+    Describe "New-JiraUser" {
+        
+        Mock Write-Debug {
+            if ($ShowDebugData)
+            {
+                Write-Host -Object "[DEBUG] $Message" -ForegroundColor Yellow
+            }
+        }
+
+        Mock Get-JiraConfigServer -ModuleName PSJira {
+            Write-Output $jiraServer
+        }
+
+        Mock Invoke-JiraMethod -ModuleName PSJira -ParameterFilter {$Method -eq 'POST' -and $URI -eq "$jiraServer/rest/api/latest/user"} {
+            if ($ShowMockData)
+            {
+                Write-Host "       Mocked Invoke-JiraMethod with POST method" -ForegroundColor Cyan
+                Write-Host "         [Method]         $Method" -ForegroundColor Cyan
+                Write-Host "         [URI]            $URI" -ForegroundColor Cyan
+            }
+            ConvertFrom-Json $testJson
+        }
+
+        # Generic catch-all. This will throw an exception if we forgot to mock something.
+        Mock Invoke-JiraMethod -ModuleName PSJira {
+            Write-Host "       Mocked Invoke-JiraMethod with no parameter filter." -ForegroundColor DarkRed
+            Write-Host "         [Method]         $Method" -ForegroundColor DarkRed
+            Write-Host "         [URI]            $URI" -ForegroundColor DarkRed
+            throw "Unidentified call to Invoke-JiraMethod"
+        }
+
+        #############
+        # Tests
+        #############
+
+        It "Creates a user in JIRA and returns a result" {
+            $newResult = New-JiraUser -UserName $testUsername -EmailAddress $testEmail -DisplayName $testDisplayName
+            $newResult | Should Not BeNullOrEmpty
+        }
+
+        It "Outputs a PSJira.User object" {
+            $newResult = New-JiraUser -UserName $testUsername -EmailAddress $testEmail -DisplayName $testDisplayName
+            (Get-Member -InputObject $newResult).TypeName | Should Be 'PSJira.User'
+            $newResult.Name | Should Be $testUsername
+            $newResult.EmailAddress | Should Be $testEmail
+            $newResult.DisplayName | Should Be $testDisplayName
+        }
+    }
+}
---
+++ Functions/New-JiraUser.ps1
@@ -0,0 +1,89 @@
+﻿function New-JiraUser
+{
+    [CmdletBinding()]
+    param(
+        [Parameter(Mandatory = $true)]
+        [String] $UserName,
+
+        [Parameter(Mandatory = $true)]
+        [Alias('Email')]
+        [String] $EmailAddress,
+        
+        [Parameter(Mandatory = $false)]
+        [String] $DisplayName,
+
+        # Should the user receive a notification e-mail?
+        [Switch] $Notify,
+
+        [Parameter(Mandatory = $false)]
+        [PSCredential] $Credential
+    )
+
+    begin
+    {
+        Write-Debug "[New-JiraUser] Reading information from config file"
+        try
+        {
+            Write-Debug "[New-JiraUser] Reading Jira server from config file"
+            $server = Get-JiraConfigServer -ConfigFile $ConfigFile -ErrorAction Stop
+        } catch {
+            $err = $_
+            Write-Debug "[New-JiraUser] Encountered an error reading configuration data."
+            throw $err
+        }
+
+        $userURL = "$server/rest/api/latest/user"
+    }
+
+    process
+    {
+        Write-Debug "[New-JiraUser] Defining properties"
+        $props = @{
+            "name" = $UserName;
+            "emailAddress" = $EmailAddress;
+        }
+
+        if ($DisplayName)
+        {
+            $props.displayName = $DisplayName
+        } else {
+            Write-Debug "[New-JiraUser] DisplayName was not specified; defaulting to UserName parameter [$UserName]"
+            $props.displayName = $UserName
+        }
+
+        if ($Notify)
+        {
+            Write-Debug "[New-JiraUser] Setting Notify property to True"
+            $props.notify = $true
+        } else {
+            Write-Debug "[New-JiraUser] Setting Notify property to False"
+            $props.notify = $false
+        }
+
+        Write-Debug "[New-JiraUser] Converting to JSON"
+        $json = ConvertTo-Json -InputObject $props
+
+        Write-Debug "[New-JiraUser] Preparing for blastoff!"
+        $result = Invoke-JiraMethod -Method Post -URI $userURL -Body $json -Credential $Credential
+
+        if ($result)
+        {
+            if ($result.errors)
+            {
+                Write-Debug "[New-JiraUser] Jira return an error result object."
+
+                $keys = (Get-Member -InputObject $result.errors | Where-Object -FilterScript {$_.MemberType -eq 'NoteProperty'}).Name
+                foreach ($k in $keys)
+                {
+                    Write-Error "Jira encountered an error: [$($k)] - $($result.errors.$k)"
+                }
+            } else {
+                # OK
+                Write-Debug "[New-JiraUser] Converting output object into a Jira user and outputting"
+                ConvertTo-JiraUser -InputObject $result
+            }
+        } else {
+            Write-Debug "[New-JiraUser] Jira returned no results to output."
+        }
+    }
+}
