04a42dd764e6379c0d92a9d75910c62c98086d7f
Joshua T <joshua@sonofone.net>
2015-08-05 09:34:59 -0500

    Added default property behavior when -Property is not supplied
    Previously, when the Property argument was not provided, Format-Jira would provide all available properties of the object, similar to Format-List *.  Now, Format-Jira will identify the default display properties (displayed when using Format-Default), and construct a table from them automatically.
    
    To use the old behavior, use Format-Jira -Property *, or just Format-Jira *

--- Functions/Format-Jira.Tests.ps1
+++ Functions/Format-Jira.Tests.ps1
@@ -51,6 +51,8 @@ InModuleScope PSJira {
 
         It "Returns only selected properties if the -Property argument is passed" {
             Mock Get-Process {
+                # Rather than actually running Get-Process, we'll use a known example of what 
+                # its output *could* be, so we can produce repeatable results.
                 [PSCustomObject] @{
                     CompanyName = 'Microsoft Corporation'
                     Handle      = 5368;
@@ -66,6 +68,55 @@ InModuleScope PSJira {
 
             Get-Process | Format-Jira -Property Name,Id | Should Be $expected1
             Get-Process | Format-Jira -Property Name,CompanyName,Id,MachineName,Handle | Should Be $expected2
+        }
+
+        It "Returns an object's default properties if the -Property argument is not passed" {
+            Mock Get-Process {
+                $obj = [PSCustomObject] @{
+                    CompanyName = 'Microsoft Corporation'
+                    Handle      = 5368;
+                    Id          = 4496;
+                    MachineName = '.'
+                    Name        = 'explorer';
+                    Path        = 'C:\Windows\Explorer.EXE';
+                }
+
+                # Since we're mocking this with a PSCustomObject, we need to define its default property set
+                [String[]] $DefaultProperties = @('Name','Id')
+                $defaultPropertySet = New-Object -TypeName System.Management.Automation.PSPropertySet -ArgumentList 'DefaultDisplayPropertySet',$DefaultProperties
+                $PSStandardMembers = [System.Management.Automation.PSMemberInfo[]] $defaultPropertySet
+                Add-Member -InputObject $obj -MemberType MemberSet -Name PSStandardMembers -Value $PSStandardMembers -Force
+
+                Write-Output $obj
+            }
+
+            $expected = "||Name||Id||$n|explorer|4496|"
+
+            Get-Process | Format-Jira | Should Be $expected
+        }
+
+        It "Returns ALL object's default properties if the -Property argument is not passed" {
+            Mock Get-Process {
+                $obj = [PSCustomObject] @{
+                    CompanyName = 'Microsoft Corporation'
+                    Handle      = 5368;
+                    Id          = 4496;
+                    MachineName = '.'
+                    Name        = 'explorer';
+                    Path        = 'C:\Windows\Explorer.EXE';
+                }
+
+                [String[]] $DefaultProperties = @('Name','Id')
+                $defaultPropertySet = New-Object -TypeName System.Management.Automation.PSPropertySet -ArgumentList 'DefaultDisplayPropertySet',$DefaultProperties
+                $PSStandardMembers = [System.Management.Automation.PSMemberInfo[]] $defaultPropertySet
+                Add-Member -InputObject $obj -MemberType MemberSet -Name PSStandardMembers -Value $PSStandardMembers -Force
+
+                Write-Output $obj
+            }
+
+            $expected = "||CompanyName||Handle||Id||MachineName||Name||Path||$n|Microsoft Corporation|5368|4496|.|explorer|C:\Windows\Explorer.EXE|"
+
+            Get-Process | Format-Jira -Property * | Should Be $expected
         }
     }
 }
