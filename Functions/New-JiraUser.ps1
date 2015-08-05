d7f3e294090f40daeb9b05fb5e5fa98622aa2303
Joshua T <joshua@sonofone.net>
2015-08-04 23:23:19 -0500

    Added comment-based help

--- Functions/New-JiraUser.ps1
+++ Functions/New-JiraUser.ps1
@@ -1,5 +1,27 @@
 ﻿function New-JiraUser
 {
+    <#
+    .Synopsis
+       Creates a new user in JIRA
+    .DESCRIPTION
+       This function creates a new user in JIRA.  By default, the new user 
+       will be notified via e-mail.
+
+       The new user's password is also randomly generated.
+    .EXAMPLE
+       New-JiraUser -UserName testUser -EmailAddress testUser@example.com
+       This example creates a new JIRA user named testUser, and sends a 
+       notification e-mail.  The user's DisplayName will be set to 
+       "testUser" since it is not specified.
+    .EXAMPLE
+       New-JiraUser -UserName testUser2 -EmailAddress testUser2@example.com -DisplayName "Test User 2"
+       This example illustrates setting a user's display name during 
+       user creation.
+    .INPUTS
+       This function does not accept pipeline input.
+    .OUTPUTS
+       [PSJira.User] The user object created
+    #>
     [CmdletBinding()]
     param(
         [Parameter(Mandatory = $true)]
