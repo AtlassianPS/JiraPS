6404fd428230b8009f76becc3ea8af6d8b654c22
Joshua T <joshua@sonofone.net>
2015-09-03 22:41:18 -0500

    Added module tests

---
+++ PSJira.Tests.ps1
@@ -0,0 +1,12 @@
+﻿$here = Split-Path -Parent $MyInvocation.MyCommand.Path
+$functions = Join-Path -Path $here -ChildPath 'Functions'
+
+Describe "PSJira" {
+    # We want to make sure that every .ps1 file in the Functions directory that isn't a Pester test has an associated Pester test.
+    # This helps keep me honest and makes sure I'm testing my code appropriately.
+    It "Includes a test for each PowerShell function in the module" {
+        Get-ChildItem -Path $functions -Filter "*.ps1" -Recurse | Where-Object -FilterScript {$_.Name -notlike '*.Tests.ps1'} | % {
+            $_.FullName -replace '.ps1','.Tests.ps1' | Should Exist
+        }
+    }
+}
