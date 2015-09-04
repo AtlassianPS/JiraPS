565aa9d08a468b34fc879d897936f42879302dd1
Joshua T <joshua@sonofone.net>
2015-09-03 21:55:07 -0500

    Added explanatory comment
    Without this comment, I can guarantee I'd forget why that line was there and remove it sometime down the road.

--- PSJira.psm1
+++ PSJira.psm1
@@ -14,4 +14,7 @@ foreach ($p in $paths)
     }
 }
 
+# Apparently, PowerShell only automatically loads format files from modules within PSModulePath.
+# This line forces the current PowerShell session to load the module format file, even if the module is saved in an unusual location.
+# If this module lives somewhere in your PSModulePath, this line is unnecessary (but it doesn't do any harm either).
 Update-FormatData -AppendPath (Join-Path -Path $moduleRoot -ChildPath 'PSJira.format.ps1xml')
