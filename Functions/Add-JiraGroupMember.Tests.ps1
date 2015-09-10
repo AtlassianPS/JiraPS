afa79ecb347b51fed4e3bd32ca850622fdedb879
Joshua T <joshua@sonofone.net>
2015-09-10 16:38:37 -0500

    Created function Add-JiraGroupMember.
    Tests are still very basic, and don't completely test the feature set, but they're as good as some of the others I've got.

---
+++ Functions/Add-JiraGroupMember.Tests.ps1
@@ -0,0 +1,126 @@
+﻿$here = Split-Path -Parent $MyInvocation.MyCommand.Path
+$sut = (Split-Path -Leaf $MyInvocation.MyCommand.Path).Replace(".Tests.", ".")
+. "$here\$sut"
+
+InModuleScope PSJira {
+
+    $ShowMockData = $false
+    $ShowDebugData = $false
+
+    $jiraServer = 'http://jiraserver.example.com'
+
+    $testGroupName = 'testGroup'
+    
+    $testUsername = 'testUsername'
+    $testUsername2 = 'testUsername2'
+
+    $testJson = @"
+{
+  "name": "$testGroupName",
+  "self": "$jiraServer/rest/api/2/group?groupname=$testGroupName",
+  "users": {
+    "size": 0,
+    "items": [],
+    "max-results": 50,
+    "start-index": 0,
+    "end-index": 0
+  },
+  "expand": "users"
+}
+"@
+
+    $testJsonUser = @"
+{
+    "self": "$jiraServer/rest/api/2/user?username=$testUsername",
+    "key": "$testUsername",
+    "name": "$testUsername",
+    "displayName": "$testUsername",
+    "emailAddress": "$testUsername@example.com",
+    "active": true
+}
+"@
+
+    $testJsonUser2 = @"
+{
+    "self": "$jiraServer/rest/api/2/user?username=$testUsername2",
+    "key": "$testUsername2",
+    "name": "$testUsername2",
+    "displayName": "$testUsername2",
+    "emailAddress": "$testUsername2@example.com",
+    "active": true
+}
+"@
+
+    Describe "Add-JiraGroupMember" {
+        
+        Mock Write-Debug -ModuleName PSJira {
+            if ($ShowDebugData)
+            {
+                Write-Host -Object "[DEBUG] $Message" -ForegroundColor Yellow
+            }
+        }
+
+        Mock Get-JiraConfigServer -ModuleName PSJira {
+            Write-Output $jiraServer
+        }
+
+        Mock Get-JiraGroup -ModuleName PSJira {
+            ConvertTo-JiraGroup (ConvertFrom-Json $testJson)
+        }
+
+        Mock Get-JiraUser -ModuleName PSJira -ParameterFilter {$UserName -eq $testUsername} {
+            ConvertTo-JiraUser (ConvertFrom-Json $testJsonUser)
+        }
+
+        Mock Get-JiraUser -ModuleName PSJira -ParameterFilter {$UserName -eq $testUsername2} {
+            ConvertTo-JiraUser (ConvertFrom-Json $testJsonUser2)
+        }
+
+        Mock Invoke-JiraMethod -ModuleName PSJira -ParameterFilter {$Method -eq 'POST' -and $URI -eq "$jiraServer/rest/api/latest/group/user?groupname=$testGroupName"} {
+            if ($ShowMockData)
+            {
+                Write-Host "       Mocked Invoke-JiraMethod with DELETE method" -ForegroundColor Cyan
+                Write-Host "         [Method]         $Method" -ForegroundColor Cyan
+                Write-Host "         [URI]            $URI" -ForegroundColor Cyan
+            }
+            # This REST method should produce no output
+        }
+
+        # Generic catch-all. This will throw an exception if we forgot to mock something.
+        Mock Invoke-JiraMethod -ModuleName PSJira {
+            Write-Host "       Mocked Invoke-JiraMethod with no parameter filter." -ForegroundColor DarkRed
+            Write-Host "         [Method]         $Method" -ForegroundColor DarkRed
+            Write-Host "         [URI]            $URI" -ForegroundColor DarkRed
+            throw "Unidentified call to Invoke-JiraMethod"
+        }
+
+        #############
+        # Tests
+        #############
+        It "Accepts a group name as a String to the -Group parameter" {
+            { Add-JiraGroupMember -Group $testGroupName -User $testUsername } | Should Not Throw
+            Assert-MockCalled -CommandName Invoke-JiraMethod -Exactly -Times 1 -Scope It
+        }
+
+        It "Accepts a PSJira.Group object to the -Group parameter" {
+            $group = Get-JiraGroup -GroupName $testGroupName
+            { Add-JiraGroupMember -Group $testGroupName -User $testUsername } | Should Not Throw
+            Assert-MockCalled -CommandName Invoke-JiraMethod -Exactly -Times 1 -Scope It
+        }
+
+        It "Accepts pipeline input from Get-JiraGroup" {
+            { Get-JiraGroup -GroupName $testGroupName | Add-JiraGroupMember -User $testUsername } | Should Not Throw
+            Assert-MockCalled -CommandName Invoke-JiraMethod -Exactly -Times 1 -Scope It
+        }
+
+        It "Adds a user to a JIRA group if the user is not already a member" {
+            { Add-JiraGroupMember -Group $testGroupName -User $testUsername } | Should Not Throw
+            Assert-MockCalled -CommandName Invoke-JiraMethod -Exactly -Times 1 -Scope It
+        }
+
+        It "Adds multiple users to a JIRA group if they are passed to the -User parameter" {
+            { Add-JiraGroupMember -Group $testGroupName -User $testUsername,$testUsername2 } | Should Not Throw
+            Assert-MockCalled -CommandName Invoke-JiraMethod -Exactly -Times 2 -Scope It
+        }
+    }
+}
---
+++ Functions/Add-JiraGroupMember.ps1
@@ -0,0 +1,102 @@
+﻿function Add-JiraGroupMember
+{
+    param(
+        [Parameter(Mandatory = $true,
+                   Position = 0,
+                   ValueFromPipeline = $true)]
+        [Alias('GroupName')]
+        [Object[]] $Group,
+
+        # Username or user object obtained from Get-JiraUser
+        [Parameter(Mandatory = $true)]
+        [Alias('UserName')]
+        [Object[]] $User,
+
+        [Parameter(Mandatory = $false)]
+        [PSCredential] $Credential,
+
+        # Whether output should be provided after invoking this function
+        [Switch] $PassThru
+    )
+
+    begin
+    {
+        Write-Debug "[Add-JiraGroupMember] Reading information from config file"
+        try
+        {
+            Write-Debug "[Add-JiraGroupMember] Reading Jira server from config file"
+            $server = Get-JiraConfigServer -ConfigFile $ConfigFile -ErrorAction Stop
+        } catch {
+            $err = $_
+            Write-Debug "[Add-JiraGroupMember] Encountered an error reading configuration data."
+            throw $err
+        }
+
+        # At present, it looks like this REST method doesn't support arrays in the Name property...
+        # in other words, a single REST call can only add a single group member to a single group.
+
+        # That's kind of annoying.
+
+        # Anyway, this builds a bunch of individual JSON strings with each username in its own Web
+        # request, which we'll loop through again in the Process block.
+
+        $userAL = New-Object -TypeName System.Collections.ArrayList
+        foreach ($u in $User)
+        {
+            Write-Debug "[Set-JiraUser] Obtaining reference to user [$u]"
+            $userObj = Get-JiraUser -InputObject $u -Credential $Credential
+
+            if ($userObj)
+            {
+                Write-Debug "[Add-JiraGroupMember] Retrieved user reference [$userObj]"
+                $thisUserJson = ConvertTo-Json -InputObject @{
+                    'name' = $userObj.Name;
+                }
+                [void] $userAL.Add($thisUserJson)
+            } else {
+                Write-Debug "[Add-JiraGroupMember] Could not identify user [$u]. Writing error message."
+                Write-Error "Unable to identify user [$u]. Check the spelling of this user and ensure that you can access it via Get-JiraUser."
+            }
+        }
+
+        $userJsons = $userAL.ToArray()
+        
+        $restUrl = "$server/rest/api/latest/group/user?groupname={0}"
+    }
+
+    process
+    {
+        foreach ($g in $Group)
+        {
+            Write-Debug "[Add-JiraGroupMember] Obtaining reference to group [$g]"
+            $groupObj = Get-JiraGroup -InputObject $g -Credential $Credential
+
+            if ($groupObj)
+            {
+                $thisRestUrl = $restUrl -f $groupObj.Name
+                Write-Debug "[Add-JiraGroupMember] Group URL: [$thisRestUrl]"
+                foreach ($json in $userJsons)
+                {
+                    Write-Debug "[Add-JiraGroupMember] Preparing for blastoff!"
+                    $result = Invoke-JiraMethod -Method Post -URI $thisRestUrl -Body $json -Credential $Credential
+                }
+
+                if ($PassThru)
+                {
+                    Write-Debug "[Add-JiraGroupMember] -PassThru specified. Obtaining a final reference to group [$g]"
+                    $groupObjNew = Get-JiraGroup -InputObject $g -Credential $Credential
+                    Write-Debug "[Add-JiraGroupMember] Outputting group [$groupObjNew]"
+                    Write-Output $groupObjNew
+                }
+            } else {
+                Write-Debug "[Add-JiraGroupMember] Could not identify group [$g]"
+                Write-Error "Unable to identify group [$g]. Check the spelling of this group and ensure that you can access it via Get-JiraGroup."
+            }
+        }
+    }
+
+    end
+    {
+        Write-Debug "[Add-JiraGroupMember] Complete"
+    }
+}
