5a42c2f65ded982b259932c5e3c433673203cfe0
Joshua T <joshua@sonofone.net>
2015-10-01 11:45:38 -0500

    Added verbose output and error checking for Update-FormatData

--- PSJira/PSJira.psm1
+++ PSJira/PSJira.psm1
@@ -25,4 +25,6 @@ foreach ($i in $items)
 # Apparently, PowerShell only automatically loads format files from modules within PSModulePath.
 # This line forces the current PowerShell session to load the module format file, even if the module is saved in an unusual location.
 # If this module lives somewhere in your PSModulePath, this line is unnecessary (but it doesn't do any harm either).
-Update-FormatData -AppendPath (Join-Path -Path $moduleRoot -ChildPath 'PSJira.format.ps1xml')
+$formatFile = Join-Path -Path $moduleRoot -ChildPath 'PSJira.format.ps1xml'
+Write-Verbose "Updating format data with file '$formatFile'"
+Update-FormatData -AppendPath $formatFile -ErrorAction Continue
