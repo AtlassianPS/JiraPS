78ba4abfa3ad9ef32ee9eb291140bbf42fdf8ef5
Joshua T <joshua@sonofone.net>
2015-10-01 12:08:39 -0500

    Added Update-ModuleManifest function.
    This should update the published version of the module manifest file to the AppVeyor build version

--- Tests/Deploy.ps1
+++ Tests/Deploy.ps1
@@ -2,14 +2,45 @@
 
 # Credit to Trevor Sullivan:
 # https://github.com/pcgeek86/PSNuGet/blob/master/deploy.ps1
+#
+# This script uses Write-Host a bit. That's not good practice, but AppVeyor
+# doesn't seem to display verbose output.
+
+#region Functions
+# Again, credit to Trevor Sullivan for this function. I'm not nearly this good
+# with regular expressions!
+function Update-ModuleManifest {
+	[CmdletBinding()]
+	param(
+		[String] $Path,
+	    [String] $BuildNumber
+	)
+
+	if ([String]::IsNullOrEmpty($Path))
+    {
+		$Path = Get-ChildItem -Path $env:APPVEYOR_BUILD_FOLDER -Include *.psd1;
+		if (!$Path)
+        {
+            throw 'Could not find a module manifest file';
+        }
+	}
+
+	$ManifestContent = Get-Content -Path $Path -Raw;
+	$ManifestContent = $ManifestContent -replace '(?<=ModuleVersion\s+=\s+'')(?<ModuleVersion>.*)(?='')', ('${{ModuleVersion}}.{0}' -f $BuildNumber);
+	Set-Content -Path $Path -Value $ManifestContent;
+
+	$ManifestContent -match '(?<=ModuleVersion\s+=\s+'')(?<ModuleVersion>.*)(?='')';
+	Write-Host -Message ('Module Version patched: ' + $Matches.ModuleVersion);
+}
+#endregion
 
 Set-StrictMode -Off
 
 $shouldDeploy = $false
 if ($env:APPVEYOR_REPO_TAG_NAME -notmatch 'release') {
-    Write-Verbose "This commit does not include the tag matching [release], so it will not be published."
+    Write-Host "This commit does not include the tag matching [release], so it will not be published."
 } elseif ($env:APPVEYOR_REPO_BRANCH -ne 'master') {
-    Write-Verbose "This commit is not to branch [master], so it will not be published."
+    Write-Host "This commit is not to branch [master], so it will not be published."
 } elseif ($PSVersionTable.PSVersion -lt '5.0.0') {
     Write-Warning "We are not running in a PowerShell 5 environment, so the module cannot be pulbished."
 } else {
@@ -17,6 +48,8 @@ if ($env:APPVEYOR_REPO_TAG_NAME -notmatch 'release') {
 }
 
 Write-Output ("AppVeyor build folder: [{0}]" -f $env:APPVEYOR_BUILD_FOLDER)
+Update-ModuleManifest -Path $env:APPVEYOR_BUILD_FOLDER\PSJira.psd1 -BuildNumber $env:APPVEYOR_BUILD_NUMBER
+
 $publishParams = @{
     Path = Join-Path -Path $env:APPVEYOR_BUILD_FOLDER -ChildPath $env:ModuleName
     NuGetApiKey = $env:PSGalleryAPIKey
@@ -30,16 +63,16 @@ if ($env:Tags)
     $publishParams.Tags = $env:Tags -split ',' | where { $_ } | foreach { $_.trim() }
 }
 
-Write-Verbose "Parameters for publishing:"
+Write-Host "Parameters for publishing:"
 foreach ($p in $publishParams.Keys)
 {
-    Write-Verbose ("  {0}`t{1}" -f $p, $publishParams.$p)
+    Write-Host ("  {0}`t{1}" -f $p, $publishParams.$p)
 }
 
 if ($shouldDeploy)
 {
-    Write-Verbose "Calling Find-Package with dummy data to download nuget-anycpu.exe"
+    Write-Host "Calling Find-Package with dummy data to download nuget-anycpu.exe"
     Find-Package -ForceBootstrap -Name zzzzzz -ErrorAction Ignore
-    Write-Verbose "Publishing module"
+    Write-Host "Publishing module"
     Publish-Module @publishParams
 }
