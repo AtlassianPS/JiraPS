a5479fcd23a5bcab13d37d4f920b8fbf47923546
Joshua T <joshua@sonofone.net>
2015-10-07 17:04:36 -0500

    Re-wrote Set-JiraIssue tests to make more sense and test more.
    Pester code coverage is up to 93.85% for this function!

--- PSJira/Functions/Set-JiraIssue.Tests.ps1
+++ PSJira/Functions/Set-JiraIssue.Tests.ps1
@@ -1,155 +1,131 @@
-$here = Split-Path -Parent $MyInvocation.MyCommand.Path
+﻿$here = Split-Path -Parent $MyInvocation.MyCommand.Path
 $sut = (Split-Path -Leaf $MyInvocation.MyCommand.Path).Replace(".Tests.", ".")
 . "$here\$sut"
 
 InModuleScope PSJira {
 
-    # This is intended to be a parameter to the test, but Pester currently does not allow parameters to be passed to InModuleScope blocks.
-    # For the time being, we'll need to hard-code this and adjust it as desired.
     $ShowMockData = $false
-
+    $ShowDebugText = $false
+    
     Describe "Set-JiraIssue" {
-
-        $jiraServer = 'http://jiraserver.example.com'
-        $issueID = 41701
-        $issueKey = 'IT-3676'
-
-        $testUsername = 'powershell-test'
-
-        Mock Get-JiraConfigServer -ModuleName PSJira {
-            Write-Output $jiraServer
+        if ($ShowDebugText)
+        {
+            Mock "Write-Debug" {
+                Write-Host "       [DEBUG] $Message" -ForegroundColor Yellow
+            }
         }
 
-        Mock Get-JiraIssue -ModuleName PSJira {
-            if ($ShowMockData)
-            {
-                Write-Host "       Mocked Get-JiraIssue" -ForegroundColor Cyan
-                Write-Host "         [Key]         $Key" -ForegroundColor Cyan
-                Write-Host "         [InputObject] $InputObject" -ForegroundColor Cyan
-            }
-
+        Mock Get-JiraIssue{
             [PSCustomObject] @{
-                Summary     = 'Test issue';
-                Description = 'Test issue from PowerShell';
-                Key         = $issueKey;
-                RestURL     = "$jiraServer/rest/api/latest/issue/$issueID";
+                'RestURL' = 'https://jira.example.com/rest/api/2/issue/12345'
             }
         }
 
-        Mock Get-JiraUser -ModuleName PSJira {
-            if ($ShowMockData)
+        # If we don't override this in a context or test, we don't want it to
+        # actually try to query a JIRA instance
+        Mock Invoke-JiraMethod {}
+
+        Context "Sanity checking" {
+            $command = Get-Command -Name Set-JiraIssue
+
+            function defParam($name)
             {
-                Write-Host "       Mocked Get-JiraUser" -ForegroundColor Cyan
-                Write-Host "         [UserName]    $UserName" -ForegroundColor Cyan
-                Write-Host "         [InputObject] $InputObject" -ForegroundColor Cyan
+                It "Has a -$name parameter" {
+                    $command.Parameters.Item($name) | Should Not BeNullOrEmpty
+                }
             }
 
-            [PSCustomObject] @{
-                Name = 'powershell-test';
-            }
-        }
+            defParam 'Issue'
+            defParam 'Summary'
+            defParam 'Description'
+            defParam 'Assignee'
+            defParam 'Credential'
+            defParam 'PassThru'
 
-        Mock Get-JiraIssue -ModuleName PSJira {
-            [PSCustomObject] @{
-                ID = $issueID;
-                Key = $issueKey;
-                RestUrl = "$jiraServer/rest/api/latest/issue/$issueID";
-            }
-        }
-
-        # Edit issue
-        Mock Invoke-JiraMethod -ModuleName PSJira -ParameterFilter {$Method -eq 'Put' -and $URI -eq "$jiraServer/rest/api/latest/issue/$issueId"} {
-            # If successful, Jira will return a 204, so no output should be produced here.
-            if ($ShowMockData)
-            {
-                Write-Host "       Mocked Invoke-JiraMethod with PUT method." -ForegroundColor Cyan
-                Write-Host "         [Method]         $Method" -ForegroundColor Cyan
-                Write-Host "         [URI]            $URI" -ForegroundColor Cyan
+            It "Supports the Key alias for the Issue parameter" {
+                $command.Parameters.Item('Issue').Aliases | Where-Object -FilterScript {$_ -eq 'Key'} | Should Not BeNullOrEmpty
             }
         }
 
-        # Assign issue
-        Mock Invoke-JiraMethod -ModuleName PSJira -ParameterFilter {$Method -eq 'Put' -and $URI -eq "$jiraServer/rest/api/latest/issue/$issueId/assignee"} {
-            # If successful, Jira will return a 204, so no output should be produced here.
-            if ($ShowMockData)
-            {
-                Write-Host "       Mocked Invoke-JiraMethod with PUT method." -ForegroundColor Cyan
-                Write-Host "         [Method]         $Method" -ForegroundColor Cyan
-                Write-Host "         [URI]            $URI" -ForegroundColor Cyan
+        Context "Behavior testing" {
+            Mock Invoke-JiraMethod {
+                if ($ShowMockData)
+                {
+                    Write-Host "       Mocked Invoke-JiraMethod" -ForegroundColor Cyan
+                    Write-Host "         [Uri]     $Uri" -ForegroundColor Cyan
+                    Write-Host "         [Method]  $Method" -ForegroundColor Cyan
+#                    Write-Host "         [Body]    $Body" -ForegroundColor Cyan
+                }
+            }
+
+            Mock Get-JiraUser {
+                [PSCustomObject] @{
+                    'Name' = 'username'
+                }
+            }
+
+            It "Modifies the summary of an issue if the -Summary parameter is passed" {
+                { Set-JiraIssue -Issue TEST-001 -Summary 'New summary' } | Should Not Throw
+                # The String in the ParameterFilter is made from the keywords
+                # we should expect to see in the JSON that should be sent,
+                # including the summary provided in the test call above.
+                Assert-MockCalled -CommandName Invoke-JiraMethod -ModuleName PSJira -Times 1 -Scope It -ParameterFilter { $Method -eq 'Put' -and $URI -like '*/rest/api/2/issue/12345' -and $Body -like '*summary*set*New summary*' }
+            }
+
+            It "Modifies the description of an issue if the -Description parameter is passed" {
+                { Set-JiraIssue -Issue TEST-001 -Description 'New description' } | Should Not Throw
+                Assert-MockCalled -CommandName Invoke-JiraMethod -ModuleName PSJira -Times 1 -Scope It -ParameterFilter { $Method -eq 'Put' -and $URI -like '*/rest/api/2/issue/12345' -and $Body -like '*description*set*New description*' }
+            }
+
+            It "Modifies the assignee of an issue if -Assignee is passed" {
+                { Set-JiraIssue -Issue TEST-001 -Assignee username } | Should Not Throw
+                Assert-MockCalled -CommandName Invoke-JiraMethod -ModuleName PSJira -Times 1 -Scope It -ParameterFilter { $Method -eq 'Put' -and $URI -like '*/rest/api/2/issue/12345/assignee' -and $Body -like '*name*username*' }
+            }
+
+            It "Unassigns an issue if 'Unassigned' is passed to the -Assignee parameter" {
+                { Set-JiraIssue -Issue TEST-001 -Assignee unassigned } | Should Not Throw
+                Assert-MockCalled -CommandName Invoke-JiraMethod -ModuleName PSJira -Times 1 -Scope It -ParameterFilter { $Method -eq 'Put' -and $URI -like '*/rest/api/2/issue/12345/assignee' -and $Body -like '*name*""*' }
+            }
+
+            It "Calls Invoke-JiraMethod twice if using Assignee and another field" {
+                { Set-JiraIssue -Issue TEST-001 -Summary 'New summary' -Assignee username } | Should Not Throw
+                Assert-MockCalled -CommandName Invoke-JiraMethod -ModuleName PSJira -Times 1 -Scope It -ParameterFilter { $Method -eq 'Put' -and $URI -like '*/rest/api/2/issue/12345' -and $Body -like '*summary*set*New summary*' }
+                Assert-MockCalled -CommandName Invoke-JiraMethod -ModuleName PSJira -Times 1 -Scope It -ParameterFilter { $Method -eq 'Put' -and $URI -like '*/rest/api/2/issue/12345/assignee' -and $Body -like '*name*username*' }
             }
         }
 
-        Mock Invoke-JiraMethod -ModuleName PSJira {
-            Write-Host "       Mocked Invoke-JiraMethod with no parameter filter." -ForegroundColor DarkRed
-            Write-Host "         [Method]         $Method" -ForegroundColor DarkRed
-            Write-Host "         [URI]            $URI" -ForegroundColor DarkRed
-            throw "Unidentified call to Invoke-JiraMethod"
-        }
+        Context "Input testing" {
+            It "Accepts an issue key for the -Issue parameter" {
+                { Set-JiraIssue -Issue TEST-001 -Summary 'Test summary - using issue key' } | Should Not Throw
+                Assert-MockCalled -CommandName Get-JiraIssue -ModuleName PSJira -Exactly -Times 1 -Scope It
+                Assert-MockCalled -CommandName Invoke-JiraMethod -ModuleName PSJira -Exactly -Times 1 -Scope It
+            }
 
-        It "Accepts an issue key for the -Issue parameter" {
-            { Set-JiraIssue -Issue $issueKey -Summary 'Test summary - Key' -Description 'This is a test of key input using the parameter directly.'} | Should Not Throw
-            Assert-MockCalled -CommandName Get-JiraIssue -ModuleName PSJira -Exactly -Times 1 -Scope It
-            Assert-MockCalled -CommandName Invoke-JiraMethod -ModuleName PSJira -Exactly -Times 1 -Scope It
-        }
+            It "Accepts an issue object for the -Issue parameter" {
+                $issue = Get-JiraIssue -Key TEST-001
+                { Set-JiraIssue -Issue $issue -Summary 'Test summary - Object' } | Should Not Throw
+                # Get-JiraIssue is called once explicitly in this test, and a
+                # second time by Set-JiraIssue
+                Assert-MockCalled -CommandName Get-JiraIssue -ModuleName PSJira -Exactly -Times 2 -Scope It
+                Assert-MockCalled -CommandName Invoke-JiraMethod -ModuleName PSJira -Exactly -Times 1 -Scope It
+            }
 
-        It "Accepts an issue object for the -Issue parameter" {
-            $issue = Get-JiraIssue -Key $issueKey
-            { Set-JiraIssue -Issue $issue -Summary 'Test summary - Object' -Description 'This is a test input using an object variable.' } | Should Not Throw
-            Assert-MockCalled -CommandName Get-JiraIssue -ModuleName PSJira -Exactly -Times 2 -Scope It
-            Assert-MockCalled -CommandName Invoke-JiraMethod -ModuleName PSJira -Exactly -Times 1 -Scope It
-        }
+            It "Accepts the output of Get-JiraObject by pipeline for the -Issue paramete" {
+                { Get-JiraIssue -Key TEST-001 | Set-JiraIssue -Summary 'Test summary - InputObject pipeline' } | Should Not Throw
+                Assert-MockCalled -CommandName Get-JiraIssue -ModuleName PSJira -Exactly -Times 2 -Scope It
+                Assert-MockCalled -CommandName Invoke-JiraMethod -ModuleName PSJira -Exactly -Times 1 -Scope It
+            }
 
-        It "Accepts the output of Get-JiraObject by pipeline for the -Issue paramete" {
-            { Get-JiraIssue -Key $issueKey | Set-JiraIssue -Summary 'Test summary - InputObject pipeline' -Description 'This is a test InputObject input using the pipeline.'} | Should Not Throw
-            Assert-MockCalled -CommandName Get-JiraIssue -ModuleName PSJira -Exactly -Times 2 -Scope It
-            Assert-MockCalled -CommandName Invoke-JiraMethod -ModuleName PSJira -Exactly -Times 1 -Scope It
-        }
+            It "Throws an exception if an invalid issue is provided" {
+                Mock Get-JiraIssue {}
+                # We're cheating a bit here and forcing Write-Error to be a
+                # terminating error.
+                { Set-JiraIssue -Key FAKE -Summary 'Test' -ErrorAction Stop } | Should Throw
+            }
 
-        It "Edits the summary and description of an issue" {
-            { Set-JiraIssue -Key $issueKey -Summary 'Test summary - IssueKey' -Description 'This is a test of editing the summary field.' } | Should Not Throw
-            Assert-MockCalled -CommandName Get-JiraIssue -ModuleName PSJira -Exactly -Times 1 -Scope It
-            Assert-MockCalled -CommandName Invoke-JiraMethod -ModuleName PSJira -Exactly -Times 1 -Scope It
-        }
-
-        It "Assigns the issue" {
-            { Set-JiraIssue -Key $issueKey -Assignee $testUsername } | Should Not Throw
-
-            # Should use Get-JiraUser to obtain Assignee
-            Assert-MockCalled -CommandName Get-JiraUser -ModuleName PSJira -Exactly -Times 1 -Scope It
-
-            # Should use Get-JiraIssue to obtain issue
-            Assert-MockCalled -CommandName Get-JiraIssue -ModuleName PSJira -Exactly -Times 1 -Scope It
-
-            # Should use Invoke-JiraMethod to update issue
-            Assert-MockCalled -CommandName Invoke-JiraMethod -ModuleName PSJira -Exactly -Times 1 -Scope It
-        }
-
-        It "Unassigns the issue if the -Assignee parameter is supplied with 'Unassigned'" {
-            { Set-JiraIssue -Key $issueKey -Assignee 'Unassigned' } | Should Not Throw
-
-            # Get-JiraUser should NOT be called for the 'Unassigned' user
-            Assert-MockCalled -CommandName Get-JiraUser -ModuleName PSJira -Exactly -Times 0 -Scope It
-
-            # Should use Get-JiraIssue to obtain issue
-            Assert-MockCalled -CommandName Get-JiraIssue -ModuleName PSJira -Exactly -Times 1 -Scope It
-
-            # Should use Invoke-JiraMethod to update issue
-            Assert-MockCalled -CommandName Invoke-JiraMethod -ModuleName PSJira -Exactly -Times 1 -Scope It
-        }
-
-        It "Edits the issue and assigns the issue if both parameters are supplied" {
-            { Set-JiraIssue -Key $issueKey -Summary 'Test summary - IssueKey and Assignee' -Description 'This is a test of editing multiple fields at once.' -Assignee $testUsername } | Should Not Throw
-
-            # Should use Get-JiraUser to obtain Assignee
-            Assert-MockCalled -CommandName Get-JiraUser -ModuleName PSJira -Exactly -Times 1 -Scope It
-
-            # Should use Get-JiraIssue to obtain issue
-            Assert-MockCalled -CommandName Get-JiraIssue -ModuleName PSJira -Exactly -Times 1 -Scope It
-
-            # Should use two separate calls to Invoke-JiraMethod to update issue and assign issue
-            Assert-MockCalled -CommandName Invoke-JiraMethod -ModuleName PSJira -Exactly -Times 2 -Scope It
+            It "Throws an exception if an invalid user is specified for the -Assignee parameter" {
+                { Set-JiraIssue -Key TEST-001 -Assignee notReal } | Should Throw
+            }
         }
     }
 }
-
-
