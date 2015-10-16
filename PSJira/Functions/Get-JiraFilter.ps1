b82690e97701ad3f9497c7de23b07709bf2da31b
Joshua T <joshua@sonofone.net>
2015-10-16 13:46:29 -0500

    Added comment-based help

--- PSJira/Functions/Get-JiraFilter.ps1
+++ PSJira/Functions/Get-JiraFilter.ps1
@@ -1,5 +1,20 @@
 ﻿function Get-JiraFilter
 {
+    <#
+    .Synopsis
+       Returns information about a filter in JIRA
+    .DESCRIPTION
+       This function returns information about a filter in JIRA, including the JQL syntax of the filter, its owner, and sharing status.
+
+       This function is only capable of returning filters by their Filter ID. This is a limitation of JIRA's REST API.  The easiest way to obtain the ID of a filter is to load the filter in the "regular" Web view of JIRA, then copy the ID from the URL of the page.
+    .EXAMPLE
+       Get-JiraFilter -Id 12345
+       Gets a reference to filter ID 12345 from JIRA
+    .INPUTS
+       [String[]] ID of the filter(s)
+    .OUTPUTS
+       [PSJira.Filter[]] Filter objects
+    #>
     [CmdletBinding()]
     param(
         [Parameter(Mandatory = $true,
