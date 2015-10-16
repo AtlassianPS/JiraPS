6ffd2de1500e012463720ca9476ce22558803203
Joshua T <joshua@sonofone.net>
2015-10-16 13:07:42 -0500

    Created function Get-JiraFilter and tests

---
+++ PSJira/Functions/Get-JiraFilter.Tests.ps1
@@ -0,0 +1,67 @@
+﻿$here = Split-Path -Parent $MyInvocation.MyCommand.Path
+$sut = (Split-Path -Leaf $MyInvocation.MyCommand.Path).Replace(".Tests.", ".")
+. "$here\$sut"
+
+InModuleScope PSJira {
+
+    $ShowMockData = $false
+    $ShowDebugText = $false
+
+    Describe 'Get-JiraFilter' {
+        if ($ShowDebugText)
+        {
+            Mock 'Write-Debug' {
+                Write-Host "       [DEBUG] $Message" -ForegroundColor Yellow
+            }
+        }
+
+        Mock Get-JiraConfigServer {
+            'https://jira.example.com'
+        }
+
+        # If we don't override this in a context or test, we don't want it to
+        # actually try to query a JIRA instance
+        Mock Invoke-JiraMethod -ModuleName PSJira {
+            if ($ShowMockData)
+            {
+                Write-Host "       Mocked Invoke-WebRequest" -ForegroundColor Cyan
+                Write-Host "         [Uri]     $Uri" -ForegroundColor Cyan
+                Write-Host "         [Method]  $Method" -ForegroundColor Cyan
+            }
+        }
+
+        Context "Sanity checking" {
+            $command = Get-Command -Name Get-JiraFilter
+
+            function defParam($name)
+            {
+                It "Has a -$name parameter" {
+                    $command.Parameters.Item($name) | Should Not BeNullOrEmpty
+                }
+            }
+
+            defParam 'Id'
+            defParam 'Credential'
+        }
+
+        Context "Behavior testing" {
+            It "Queries JIRA for a filter with a given ID" {
+                { Get-JiraFilter -Id 12345 } | Should Not Throw
+                Assert-MockCalled -CommandName Invoke-JiraMethod -ModuleName PSJira -Exactly -Times 1 -Scope It -ParameterFilter {$Method -eq 'Get' -and $URI -like '*/rest/api/*/filter/12345'}
+            }
+        }
+
+        Context "Input testing" {
+            It "Accepts a filter ID for the -Filter parameter" {
+                { Get-JiraFilter -Id 12345 } | Should Not Throw
+                Assert-MockCalled -CommandName Invoke-JiraMethod -ModuleName PSJira -Exactly -Times 1 -Scope It
+            }
+
+            It "Accepts multiple filter IDs to the -Filter parameter" {
+                { Get-JiraFilter -Id '12345','67890' } | Should Not Throw
+                Assert-MockCalled -CommandName Invoke-JiraMethod -ModuleName PSJira -Exactly -Times 1 -Scope It -ParameterFilter {$Method -eq 'Get' -and $URI -like '*/rest/api/*/filter/12345'}
+                Assert-MockCalled -CommandName Invoke-JiraMethod -ModuleName PSJira -Exactly -Times 1 -Scope It -ParameterFilter {$Method -eq 'Get' -and $URI -like '*/rest/api/*/filter/67890'}
+            }
+        }
+    }
+}
---
+++ PSJira/Functions/Get-JiraFilter.ps1
@@ -0,0 +1,59 @@
+﻿function Get-JiraFilter
+{
+    [CmdletBinding()]
+    param(
+        [Parameter(Mandatory = $true,
+                   Position = 0)]
+        [String[]] $Id,
+
+        # Credentials to use to connect to Jira
+        [Parameter(Mandatory = $false)]
+        [System.Management.Automation.PSCredential] $Credential
+    )
+
+    begin
+    {
+        Write-Debug "[Get-JiraFilter] Reading server from config file"
+        try
+        {
+            $server = Get-JiraConfigServer -ConfigFile $ConfigFile -ErrorAction Stop
+        } catch {
+            $err = $_
+            Write-Debug "[Get-JiraFilter] Encountered an error reading the Jira server."
+            throw $err
+        }
+
+        $uri = "$server/rest/api/latest/filter/{0}"
+    }
+
+    process
+    {
+        foreach ($i in $Id)
+        {
+            Write-Debug "[Get-JiraFilter] Processing filter [$i]"
+            $thisUri = $uri -f $i
+            Write-Debug "[Get-JiraFilter] Filter URI: [$thisUri]"
+            Write-Debug "[Get-JiraFilter] Preparing for blast off!"
+            $result = Invoke-JiraMethod -Method Get -URI $thisUri -Credential $Credential
+
+            if ($result)
+            {
+#                Write-Debug "[Get-JiraPriority] Converting REST result to JiraPriority object"
+#                $obj = ConvertTo-JiraPriority -InputObject $result
+
+#                Write-Debug "[Get-JiraPriority] Outputting result"
+#                Write-Output $obj
+                Write-Debug "Outputting result"
+                Write-Output $result
+            } else {
+                Write-Debug "[Get-JiraFilter] Invoke-JiraFilter returned no results to output."
+            }
+
+        }
+    }
+
+    end
+    {
+        Write-Debug "[Get-JiraFilter] Complete"
+    }
+}
