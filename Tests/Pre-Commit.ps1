d4312ba2cc74a2a26d9e4fa64afc07263a7bdcec
Joshua T <joshua@sonofone.net>
2015-12-09 15:37:14 -0600

    Added pre-commit hook

---
+++ Tests/Pre-Commit.ps1
@@ -0,0 +1,30 @@
+﻿Write-Host
+Write-Host "Starting Pre-Commit Hooks..." -ForegroundColor Cyan
+try {
+    $results = Invoke-Pester -PassThru
+    if ($results.FailedCount -gt 0)
+    {
+        Write-Error "Pester failed $($results.FailedCount) tests."
+        Write-Host
+        Write-Host "Summary of failed tests:" -ForegroundColor White -BackgroundColor DarkRed
+        Write-Host "------------------------" -ForegroundColor White -BackgroundColor DarkRed
+
+        $failedTests = $pesterResults.TestResult | Where-Object -FilterScript {$_.Passed -ne $true}
+        $failedTests | ForEach-Object {
+            $test = $_
+            [PSCustomObject] @{
+                Describe = $Test.Describe;
+                Context = $Test.Context;
+                Name = "It `"$($test.Name)`"";
+                Result = $test.Result;
+            }
+        } | Sort-Object -Property Describe,Context,Name,Result | Format-List
+
+        exit 1
+    }
+}
+catch {
+    $err = $_
+    Write-Error -Message $_
+    exit 1
+}
