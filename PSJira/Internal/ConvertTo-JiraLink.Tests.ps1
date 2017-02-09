82c676a06801c559a8d103f7301e80b9d2d6e638
Oliver Lipkau <oliver@lipkau.net>
2017-02-09 12:35:11 +0100

    added ConvertTo-JiraLink & UnitTests

---
+++ PSJira/Internal/ConvertTo-JiraLink.Tests.ps1
@@ -0,0 +1,61 @@
+﻿$here = Split-Path -Parent $MyInvocation.MyCommand.Path
+$sut = (Split-Path -Leaf $MyInvocation.MyCommand.Path).Replace(".Tests.", ".")
+. "$here\$sut"
+
+InModuleScope PSJira {
+    Describe "ConvertTo-JiraLink" {
+        function defProp($obj, $propName, $propValue)
+        {
+            It "Defines the '$propName' property" {
+                $obj.$propName | Should Be $propValue
+            }
+        }
+
+        $jiraServer = 'http://jiraserver.example.com'
+        $LinkID = "10000"
+
+        $sampleJson = @"
+{
+    "id": 10000,
+    "self": "http://jiraserver.example.com/rest/api/issue/MKY-1/remotelink/10000",
+    "globalId": "system=http://www.mycompany.com/support&id=1",
+    "application": {
+        "type": "com.acme.tracker",
+        "name": "My Acme Tracker"
+    },
+    "relationship": "causes",
+    "object": {
+        "url": "http://www.mycompany.com/support?id=1",
+        "title": "TSTSUP-111",
+        "summary": "Crazy customer support issue",
+        "icon": {
+            "url16x16": "http://www.mycompany.com/support/ticket.png",
+            "title": "Support Ticket"
+        },
+        "status": {
+            "resolved": true,
+            "icon": {
+                "url16x16": "http://www.mycompany.com/support/resolved.png",
+                "title": "Case Closed",
+                "link": "http://www.mycompany.com/support?id=1&details=closed"
+            }
+        }
+    }
+}
+"@
+        $sampleObject = ConvertFrom-Json2 -InputObject $sampleJson
+
+        $r = ConvertTo-JiraLink -InputObject $sampleObject
+
+        It "Creates a PSObject out of JSON input" {
+            $r | Should Not BeNullOrEmpty
+        }
+
+        It "Sets the type name to PSJira.Link" {
+            (Get-Member -InputObject $r).TypeName | Should Be 'PSJira.Link'
+        }
+
+        defProp $r 'id' $LinkId
+        defProp $r 'RestUrl' "$jiraServer/rest/api/issue/MKY-1/remotelink/10000"
+    }
+}
