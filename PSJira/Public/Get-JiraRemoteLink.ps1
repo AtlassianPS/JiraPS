e0cd8ca194fd676353a1f2f336bac64ea334c7d3
Oliver Lipkau <oliver@lipkau.net>
2017-02-09 10:58:18 +0100

    refactored
    refactored according to current PSJira styling

--- PSJira/Public/Get-JiraRemoteLink.ps1
+++ PSJira/Public/Get-JiraRemoteLink.ps1
@@ -1,44 +1,77 @@
 function Get-JiraRemoteLink
 {
-    #https://docs.atlassian.com/jira/REST/latest/#d2e928
-    #Get-JiraRemoteLink -apiURi $JiraRestURi -cred $cred -issueID "HCF-966"
-    #Get-JiraRemoteLink -apiURi $JiraRestURi -cred $cred -issueID "HCF-120"
+    <#
+    .Synopsis
+       Returns a remote link from a Jira issue
+    .DESCRIPTION
+       This function returns information on remote links from a  JIRA issue.
+    .EXAMPLE
+       Get-JiraRemoteLink -Issue Project1-1000 -Credential $cred
+       Returns information about all remote links from the issue "Project1-1000"
+    .EXAMPLE
+       Get-JiraRemoteLink -Issue Project1-1000 -LinkId 100000 -Credential $cred
+       Returns information about a specific remote link from the issue "Project1-1000"
+    .INPUTS
+       [Object[]] The issue to look up in JIRA. This can be a String or a PSJira.Issue object.
+    .OUTPUTS
+       [PSJira.Link]
+    #>
     [CmdletBinding()]
     param(
         [Parameter(ValueFromPipelineByPropertyName = $true,
-                   ParameterSetName = 'ByIssueKey',
                    ValueFromPipeline = $true,
                    Mandatory = $true,
                    Position = 0
         )]
-        [ValidateNotNullOrEmpty()]
-        [String[]]$Key,
+        [Alias("Key")]
+        [String[]]$Issue,
 
         # Get a single link by it's id
-        [int]$linkId,
+        [Int[]]$LinkId,
 
         # Credentials to use to connect to Jira
         [Parameter(Mandatory = $false)]
-        [System.Management.Automation.PSCredential] $Credential
+        [PSCredential] $Credential
     )
 
     Begin
     {
         Write-Debug "[Get-JiraRemoteLink] Reading server from config file"
         $server = Get-JiraConfigServer -ConfigFile $ConfigFile -ErrorAction Stop
+
+        Write-Debug "[Get-JiraRemoteLink] ParameterSetName=$($PSCmdlet.ParameterSetName)"
+
+        Write-Debug "[Get-JiraRemoteLink] Building URI for REST call"
+        $linkUrl = "$server/rest/api/latest/issue/{0}/remotelink"
     }
 
     Process
     {
-        foreach ($k in $key)
+        foreach ($k in $Issue)
         {
             Write-Debug "[Get-JiraRemoteLink] Processing issue key [$k]"
-            $issueURL = "$($server)/rest/api/latest/issue/${k}/remotelink"
-            if ($linkId)
-                { $issueURL += "/$linkId" }
+            foreach ($l in $LinkId)
+            {
+                $thisUrl = $linkUrl -f $k
 
-            Write-Debug "[Get-JiraRemoteLink] Preparing for blastoff!"
-            Invoke-JiraMethod -Method Get -URI $issueURL -Credential $Credential
+                if ($linkId)
+                    { $thisUrl += "/$l" }
+
+                Write-Debug "[Get-JiraRemoteLink] Preparing for blastoff!"
+                $result = Invoke-JiraMethod -Method Get -URI $thisUrl -Credential $Credential
+
+                if ($result)
+                {
+                    Write-Debug "[Get-JiraRemoteLink] Converting results to PSJira.Group"
+                    $obj = ConvertTo-JiraLink -InputObject $result
+
+                    Write-Debug "[Get-JiraRemoteLink] Outputting results"
+                    Write-Output $obj
+                } else {
+                    Write-Debug "[Get-JiraRemoteLink] No results were returned from JIRA"
+                    Write-Verbose "No results were returned from JIRA."
+                }
+            }
         }
     }
 
