1d50684bceadc41c1262e447085f16591173d0fe
Axel Gluth <axel.gluth@innogames.com>
2017-02-17 17:08:33 +0100

    Moved writing to $MyInvocation.MyCommand.Module.PrivateData to private function.

---
+++ PSJira/Internal/Export-JiraConfigServerModulePrivateData.ps1
@@ -0,0 +1,35 @@
+﻿function Export-JiraConfigServerModulePrivateData
+{
+    <#
+    .Synopsis
+       Defines the configured URL for the JIRA server
+    .DESCRIPTION
+       This function defines the configured URL for the JIRA server that PSJira should manipulate. By default, this is stored in a config.xml file at the module's root path.
+    .EXAMPLE
+       Set-JiraConfigServer 'https://jira.example.com:8080'
+       This example defines the server URL of the JIRA server configured in the PSJira config file.
+    .EXAMPLE
+       Set-JiraConfigServer -Server 'https://jira.example.com:8080' -ConfigFile C:\jiraconfig.xml
+       This example defines the server URL of the JIRA server configured at C:\jiraconfig.xml.
+    .INPUTS
+       This function does not accept pipeline input.
+    .OUTPUTS
+       [System.String]
+    .NOTES
+       Support for multiple configuration files is limited at this point in time, but enhancements are planned for a future update.
+    #>
+    [CmdletBinding()]
+    param(
+        # The base URL of the Jira instance.
+        [Parameter(Mandatory = $true,
+                   Position = 0)]
+        [ValidateNotNullOrEmpty()]
+        [Alias('Uri')]
+        [String] $Server
+    )
+    if (-not $MyInvocation.MyCommand.Module.PrivateData)
+    {
+        $MyInvocation.MyCommand.Module.PrivateData = @{}
+    }
+    $MyInvocation.MyCommand.Module.PrivateData.ActiveConfigServer = $Server
+}
---
+++ PSJira/Internal/Import-JiraConfigServerModulePrivateData.ps1
@@ -0,0 +1,34 @@
+﻿function Import-JiraConfigServerModulePrivateData
+{
+    <#
+    .Synopsis
+       Obtains the configured URL for the JIRA server
+    .DESCRIPTION
+       This function returns the configured URL for the JIRA server that PSJira should manipulate. By default, this is stored in a config.xml file at the module's root path.
+    .EXAMPLE
+       Get-JiraConfigServer
+       Returns the server URL of the JIRA server configured in the PSJira config file.
+    .EXAMPLE
+       Get-JiraConfigServer -ConfigFile C:\jiraconfig.xml
+       Returns the server URL of the JIRA server configured at C:\jiraconfig.xml.
+    .INPUTS
+       This function does not accept pipeline input.
+    .OUTPUTS
+       [System.String]
+    .NOTES
+       Support for multiple configuration files is limited at this point in time, but enhancements are planned for a future update.
+    #>
+    [CmdletBinding()]
+    [OutputType([System.String])]
+    param(
+    )
+    if (-not $MyInvocation.MyCommand.Module.PrivateData)
+    {
+        $MyInvocation.MyCommand.Module.PrivateData = @{}
+    }
+    if ($MyInvocation.MyCommand.Module.PrivateData.ActiveConfigServer)
+    {
+        $ActiveConfigServer = $MyInvocation.MyCommand.Module.PrivateData.ActiveConfigServer
+        Write-Output $ActiveConfigServer
+    }
+}
