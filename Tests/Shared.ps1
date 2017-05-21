52de00b03e19eddc3fec5739f365d938d771da4b
Joshua T <joshua@sonofone.net>
2017-05-21 14:21:07 -0500

    Set $SuppressImportModule after import
    This should prevent each individual test from needing to re-define the variable.

--- Tests/Shared.ps1
+++ Tests/Shared.ps1
@@ -4,12 +4,19 @@ $ModuleName = 'PSJira'
 $ModuleManifestPath = "$PSScriptRoot\..\$ModuleName\$ModuleManifestName.psd1"
 $RootModule = "$PSScriptRoot\..\$ModuleName\$ModuleName.psm1"
 
+# The first time this is called, the module will be forcibly (re-)imported.
+# After importing it once, the $SuppressImportModule flag should prevent
+# the module from being imported again for each test file.
+
 if (-not (Get-Module -Name $ModuleName -ErrorAction SilentlyContinue) -or (!$SuppressImportModule)) {
     # If we import the .psd1 file, Pester has issues where it detects multiple
     # modules named PSJira. Importing the .psm1 file seems to correct this.
 
     # -Scope Global is needed when running tests from within a CI environment
     Import-Module $RootModule -Scope Global -Force
+
+    # Set to true so we don't need to import it again for the next test
+    $SuppressImportModule = $true
 }
 
 function defProp($obj, $propName, $propValue)
