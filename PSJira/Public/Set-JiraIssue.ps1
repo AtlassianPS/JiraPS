3eeda22d3e4d7b57243d21a33a70977581e0abf8
Michael Dejulia <Dejulia489@users.noreply.github.com>
2017-05-22 11:41:29 -0700

    Add FixVersion Support to Set-JIRAIssue - Issue #40
    Corrected Get-JiraIssue mistake from prior commit.

--- PSJira/Public/Set-JiraIssue.ps1
+++ PSJira/Public/Set-JiraIssue.ps1
@@ -41,6 +41,10 @@ function Set-JiraIssue
         [Parameter(Mandatory = $false)]
         [String] $Description,
 
+        # New FixVersion of the issue
+        [Parameter(Mandatory = $false)]
+        [System.Collections.Hashtable] $FixVersion,
+
         # New assignee of the issue. Enter 'Unassigned' to unassign the issue.
         [Parameter(Mandatory = $false)]
         [Object] $Assignee,
@@ -67,7 +71,7 @@ function Set-JiraIssue
     {
         Write-Debug "[Set-JiraIssue] Checking to see if we have any operations to perform"
         $fieldNames = $Fields.Keys
-        if (-not ($Summary -or $Description -or $Assignee -or $Label -or $fieldNames))
+        if (-not ($Summary -or $Description -or $Assignee -or $Label -or $FixVersion -or $fieldNames))
         {
             Write-Verbose "Nothing to do."
             return
@@ -137,6 +141,15 @@ function Set-JiraIssue
                     $actOnIssueUri = $true
                 }
 
+                If($FixVersion)
+                {
+                    $issueProps.update.fixVersions = @()
+                    $issueProps.update.fixVersions += @{
+                        'add' = $FixVersion;
+                    }
+                    $actOnIssueUri = $true
+                }
+
                 if ($Fields)
                 {
                     Write-Debug "[Set-JiraIssue] Validating field names"
@@ -168,7 +181,6 @@ function Set-JiraIssue
 
                 if ($validAssignee)
                 {
-
                     $assigneeProps =  @{
                         'name' = $assigneeString;
                     }
@@ -213,7 +225,6 @@ function Set-JiraIssue
                     Write-Debug "[Set-JiraIssue] PassThru was specified. Obtaining updated reference to issue"
                     Get-JiraIssue -Key $issueObj.Key -Credential $Credential
                 }
-
             } else {
                 Write-Debug "[Set-JiraIssue] Unable to identify issue [$i]. Writing error message."
                 Write-Error "Unable to identify issue [$i]"
@@ -226,5 +237,3 @@ function Set-JiraIssue
         Write-Debug "[Set-JiraIssue] Complete"
     }
 }
-
-
