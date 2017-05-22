909b0b43e9f3c2223ec5beae1dba39914c6d7a77
Michael Dejulia <Dejulia489@users.noreply.github.com>
2017-05-22 11:21:07 -0700

    Add FixVersion Support to Set-JIRAIssue - Issue #40
    Added FixVersion param to Set-JIRAIssue. Will address new FixVersion creation with a separate function

--- PSJira/Public/Get-JiraIssue.ps1
+++ PSJira/Public/Get-JiraIssue.ps1
@@ -1,217 +1,244 @@
-function Get-JiraIssue
+function Set-JiraIssue
 {
     <#
     .Synopsis
-       Returns information about an issue in JIRA.
+       Modifies an existing issue in JIRA
     .DESCRIPTION
-       This function obtains references to issues in JIRA.
-
-       This function can be used to directly query JIRA for a specific issue key or internal issue ID. It can also be used to query JIRA for issues matching a specific criteria using JQL (Jira Query Language).
-
-       For more details on JQL syntax, see this articla from Atlassian: https://confluence.atlassian.com/display/JIRA/Advanced+Searching
-
-       Output from this function can be piped to various other functions in this module, including Set-JiraIssue, Add-JiraIssueComment, and Invoke-JiraIssueTransition.
+       This function modifies an existing isue in JIRA.  This can include changing
+       the issue's summary or description, or assigning the issue.
     .EXAMPLE
-       Get-JiraIssue -Key TEST-001
-       This example returns a reference to JIRA issue TEST-001.
+       Set-JiraIssue -Issue TEST-01 -Summary 'Modified issue summary' -Description 'This issue has been modified by PowerShell'
+       This example changes the summary and description of the JIRA issue TEST-01.
     .EXAMPLE
-       Get-JiraIssue "TEST-002" | Add-JiraIssueComment "Test comment from PowerShell"
-       This example illustrates pipeline use from Get-JiraIssue to Add-JiraIssueComment.
+       $issue = Get-JiraIssue TEST-01
+       $issue | Set-JiraIssue -Description "$($issue.Description)`n`nEdit: Also foo."
+       This example appends text to the end of an existing issue description by using
+       Get-JiraIssue to obtain a reference to the current issue and description.
     .EXAMPLE
-       Get-JiraIssue -Query 'project = "TEST" AND created >= -5d'
-       This example illustrates using the Query parameter and JQL syntax to query Jira for matching issues.
+       Set-JiraIssue -Issue TEST-01 -Assignee 'Unassigned'
+       This example removes the assignee from JIRA issue TEST-01.
     .INPUTS
-       This function can accept PSJira.Issue objects, Strings, or Objects via the pipeline.
-
-       * If a PSJira.Issue object is passed, this function returns a new reference to the same issue.
-       * If a String is passed, this function searches for an issue with that issue key or internal ID.
-       * If an Object is passed, this function invokes its ToString() method and treats it as a String.
+       [PSJira.Issue[]] The JIRA issue that should be modified
     .OUTPUTS
-       This function outputs the PSJira.Issue object retrieved.
-    .NOTES
-       This function requires either the -Credential parameter to be passed or a persistent JIRA session. See New-JiraSession for more details.  If neither are supplied, this function will run with anonymous access to JIRA.
+       If the -PassThru parameter is provided, this function will provide a reference
+       to the JIRA issue modified.  Otherwise, this function does not provide output.
     #>
-    [CmdletBinding(DefaultParameterSetName = 'ByIssueKey')]
+    [CmdletBinding(DefaultParameterSetName = 'ByInputObject')]
     param(
-        [Parameter(ParameterSetName = 'ByIssueKey',
-                   Mandatory = $true,
-                   Position = 0)]
-        [ValidateNotNullOrEmpty()]
-        [String[]] $Key,
+        # Issue key or PSJira.Issue object returned from Get-JiraIssue
+        [Parameter(Mandatory = $true,
+                   Position = 0,
+                   ValueFromPipeline = $true,
+                   ValueFromPipelineByPropertyName = $true)]
+        [Alias('Key')]
+        [Object[]] $Issue,
 
-        [Parameter(ParameterSetName = 'ByInputObject',
-                   Mandatory = $true,
-                   Position = 0)]
-        [Object[]] $InputObject,
+        # New summary of the issue
+        [Parameter(Mandatory = $false)]
+        [String] $Summary,
 
-        [Parameter(ParameterSetName = 'ByJQL',
-                   Mandatory = $true)]
-        [Alias('JQL')]
-        [String] $Query,
+        # New description of the issue
+        [Parameter(Mandatory = $false)]
+        [String] $Description,
 
-        [Parameter(ParameterSetName = 'ByFilter')]
-        [Object] $Filter,
+        # New FixVersion of the issue
+        [Parameter(Mandatory = $false)]
+        [System.Collections.Hashtable] $FixVersion,
 
-        [Parameter(ParameterSetName = 'ByJQL')]
-        [Parameter(ParameterSetName = 'ByFilter')]
-        [Int] $StartIndex = 0,
+        # New assignee of the issue. Enter 'Unassigned' to unassign the issue.
+        [Parameter(Mandatory = $false)]
+        [Object] $Assignee,
 
-        [Parameter(ParameterSetName = 'ByJQL')]
-        [Parameter(ParameterSetName = 'ByFilter')]
-        [Int] $MaxResults = 0,
+        # Labels to be set on the issue. These wil overwrite any existing
+        # labels on the issue. For more granular control over issue labels,
+        # use Set-JiraIssueLabel.
+        [String[]] $Label,
 
-        # How many issues should be returned per call to JIRA. This parameter
-        # only has effect if $MaxResults is not provided or set to 0. Normally,
-        # you should not need to adjust this parameter, but if the REST calls
-        # take a long time, try playing with different values here.
-        [Parameter(ParameterSetName = 'ByJQL')]
-        [Parameter(ParameterSetName = 'ByFilter')]
-        [Int] $PageSize = 50,
+        # Any additional fields that should be updated
+        [System.Collections.Hashtable] $Fields,
+
+        [ValidateScript({Test-Path $_})]
+        [String] $ConfigFile,
 
         # Credentials to use to connect to Jira
         [Parameter(Mandatory = $false)]
-        [System.Management.Automation.PSCredential] $Credential
+        [System.Management.Automation.PSCredential] $Credential,
+
+        [Switch] $PassThru
     )
 
     begin
     {
-        Write-Debug "[Get-JiraIssue] Reading server from config file"
+        Write-Debug "[Set-JiraIssue] Checking to see if we have any operations to perform"
+        $fieldNames = $Fields.Keys
+        if (-not ($Summary -or $Description -or $Assignee -or $Label -or $FixVersion -or $fieldNames))
+        {
+            Write-Verbose "Nothing to do."
+            return
+        }
+
+        Write-Debug "[Set-JiraIssue] Reading server from config file"
         $server = Get-JiraConfigServer -ConfigFile $ConfigFile -ErrorAction Stop
 
-        Write-Debug "[Get-JiraIssue] ParameterSetName=$($PSCmdlet.ParameterSetName)"
-
-        $psName = $PSCmdlet.ParameterSetName
-
-        if (($psName -eq 'ByJQL' -or $psName -eq 'ByFilter') -and $MaxResults -eq 0)
+        if ($Assignee)
         {
-            Write-Debug "[Get-JiraIssue] Using loop mode to obtain all results"
-            $MaxResults = 1
-            $loopMode = $true
-        } else {
-            $loopMode = $false
+            Write-Debug "[Set-JiraIssue] Testing Assignee type"
+            if ($Assignee -eq 'Unassigned')
+            {
+                Write-Debug "[Set-JiraIssue] 'Unassigned' String passed. Issue will be assigned to no one."
+                $assigneeString = ""
+                $validAssignee = $true
+            } else {
+                Write-Debug "[Set-JiraIssue] Attempting to obtain Jira user [$Assignee]"
+                $assigneeObj = Get-JiraUser -InputObject $Assignee -Credential $Credential
+                if ($assigneeObj)
+                {
+                    Write-Debug "[Set-JiraIssue] User found (name=[$($assigneeObj.Name)],RestUrl=[$($assigneeObj.RestUrl)])"
+                    $assigneeString = $assigneeObj.Name
+                    $validAssignee = $true
+                } else {
+                    Write-Debug "[Set-JiraIssue] Unable to obtain Assignee. Exception will be thrown."
+                    throw "Unable to validate Jira user [$Assignee]. Use Get-JiraUser for more details."
+                }
+            }
         }
+
+        Write-Debug "[Set-JiraIssue] Completed Begin block."
     }
 
     process
     {
-        if ($PSCmdlet.ParameterSetName -eq 'ByIssueKey')
+        foreach ($i in $Issue)
         {
-            foreach ($k in $Key)
+            $actOnIssueUri = $false
+            $actOnAssigneeUri = $false
+
+            Write-Debug "[Set-JiraIssue] Obtaining reference to issue"
+            $issueObj = Get-JiraIssue -InputObject $i -Credential $Credential
+
+            if ($issueObj)
             {
-                Write-Debug "[Get-JiraIssue] Processing issue key [$k]"
-                $issueURL = "$($server)/rest/api/latest/issue/${k}?expand=transitions"
-
-                Write-Debug "[Get-JiraIssue] Preparing for blastoff!"
-                $result = Invoke-JiraMethod -Method Get -URI $issueURL -Credential $Credential
-
-                if ($result)
-                {
-                    Write-Debug "[Get-JiraIssue] Converting REST result to Jira object"
-                    $obj = ConvertTo-JiraIssue -InputObject $result
-
-                    Write-Debug "[Get-JiraIssue] Outputting result"
-                    Write-Output $obj
-                } else {
-                    Write-Debug "[Get-JiraIssue] Invoke-JiraMethod returned no results to output."
-                }
-            }
-        } elseif ($PSCmdlet.ParameterSetName -eq 'ByInputObject') {
-            foreach ($i in $InputObject)
-            {
-                Write-Debug "[Get-JiraIssue] Processing InputObject [$i]"
-                if ((Get-Member -InputObject $i).TypeName -eq 'PSJira.Issue')
-                {
-                    Write-Debug "[Get-JiraIssue] Issue parameter is a PSJira.Issue object"
-                    $issueKey = $i.Key
-                } else {
-                    $issueKey = $i.ToString()
-                    Write-Debug "[Get-JiraIssue] Issue key is assumed to be [$issueKey] via ToString()"
+                $issueProps = @{
+                    'update' = @{}
                 }
 
-                Write-Debug "[Get-JiraIssue] Invoking myself with the Key parameter set to search for issue [$issueKey]"
-                $issueObj = Get-JiraIssue -Key $issueKey -Credential $Credential
-                Write-Debug "[Get-JiraIssue] Returned from invoking myself; outputting results"
-                Write-Output $issueObj
-            }
-        } elseif ($PSCmdlet.ParameterSetName -eq 'ByJQL') {
-
-            Write-Debug "[Get-JiraIssue] Escaping query and building URL"
-            $escapedQuery = [System.Web.HttpUtility]::UrlPathEncode($Query)
-            $issueURL = "$($server)/rest/api/latest/search?jql=$escapedQuery&validateQuery=true&expand=transitions&startAt=$StartIndex&maxResults=$MaxResults"
-
-            Write-Debug "[Get-JiraIssue] Preparing for blastoff!"
-            $result = Invoke-JiraMethod -Method Get -URI $issueURL -Credential $Credential
-
-            if ($result)
-            {
-                # {"startAt":0,"maxResults":50,"total":0,"issues":[]}
-
-                if ($loopMode)
+                if ($Summary)
                 {
-                    $totalResults = $result.total
+                    # Update properties need to be passed to JIRA as arrays
+                    $issueProps.update.summary = @()
+                    $issueProps.update.summary += @{
+                        'set' = $Summary;
+                    }
+                    $actOnIssueUri = $true
+                }
 
-                    Write-Debug "[Get-JiraIssue] Paging through all issues (loop mode)"
-                    $allIssues = New-Object -TypeName System.Collections.ArrayList
+                if ($Description)
+                {
+                    $issueProps.update.description = @()
+                    $issueProps.update.description += @{
+                        'set' = $Description;
+                    }
+                    $actOnIssueUri = $true
+                }
 
-                    for ($i = 0; $i -lt $totalResults; $i = $i + $PageSize)
+                If($FixVersion)
+                {
+                    $issueProps.update.fixVersions = @()
+                    $issueProps.update.fixVersions += @{
+                        'add' = $FixVersion;
+                    }
+                    $actOnIssueUri = $true
+                
+                }
+
+                if ($Fields)
+                {
+                    Write-Debug "[Set-JiraIssue] Validating field names"
+                    foreach ($k in $Fields.Keys)
                     {
-                        $percentComplete = ($i / $totalResults) * 100
-                        Write-Progress -Activity 'Get-JiraIssue' -Status "Obtaining issues ($i - $($i + $PageSize))..." -PercentComplete $percentComplete
-                        Write-Debug "[Get-JiraIssue] Obtaining issues $i - $($i + $PageSize)..."
-                        $thisSection = Get-JiraIssue -Query $Query -StartIndex $i -MaxResults $PageSize -Credential $Credential
-                        foreach ($t in $thisSection)
+                        $name = $k
+                        $value = $Fields.$k
+                        Write-Debug "[Set-JiraIssue] Attempting to identify field (name=[$name], value=[$value])"
+
+                        $f = Get-JiraField -Field $name -Credential $Credential
+                        if ($f)
                         {
-                            [void] $allIssues.Add($t)
+                            # For some reason, this was coming through as a hashtable instead of a String,
+                            # which was causing ConvertTo-Json to crash later.
+                            # Not sure why, but this forces $id to be a String and not a hashtable.
+                            $id = "$($f.ID)"
+                            Write-Debug "[Set-JiraIssue] Field [$name] was identified as ID [$id]"
+                            $issueProps.update.$id = @()
+                            $issueProps.update.$id += @{
+                                'set' = $value;
+                            }
+                            $actOnIssueUri = $true
+                        } else {
+                            Write-Debug "[Set-JiraIssue] Field [$name] could not be identified in Jira"
+                            throw "Unable to identify field [$name] from -Fields hashtable. Use Get-JiraField for more information."
                         }
                     }
-                    Write-Progress -Activity 'Get-JiraIssue' -Status 'Obtaining issues' -Completed
-                    Write-Output ($allIssues.ToArray())
-                } elseif ($result.total -gt 0) {
-                    Write-Debug "[Get-JiraIssue] Converting REST result to Jira issue"
-                    $obj = ConvertTo-JiraIssue -InputObject $result.issues
+                }
 
-                    Write-Debug "[Get-JiraIssue] Outputting result"
-                    Write-Output $obj
-                } else {
-                    Write-Debug "[Get-JiraIssue] No results were found for the specified query"
-                    Write-Verbose "No results were found for the query [$Query]"
-                }
-            } else {
-                Write-Debug "[Get-JiraIssue] Invoke-JiraMethod returned no results"
-            }
-        } elseif ($PSCmdlet.ParameterSetName -eq 'ByFilter') {
-            $filterObj = Get-JiraFilter -InputObject $Filter -Credential $Credential
-            if ($filterObj)
-            {
-                $jql = $filterObj.JQL
-                Write-Debug "[Get-JiraIssue] Invoking myself with filter JQL: [$jql]"
+                if ($validAssignee)
+                {
 
-                # MaxResults would have been set to 1 in the Begin block if it
-                # was not supplied as a parameter. We don't want to explicitly
-                # invoke this method recursively with a MaxResults value of 1
-                # if it wasn't initially provided to us.
-                if ($loopMode)
-                {
-                    $result = Get-JiraIssue -Query $jql -Credential $Credential
-                } else {
-                    $result = Get-JiraIssue -Query $jql -Credential $Credential -MaxResults $MaxResults
+                    $assigneeProps =  @{
+                        'name' = $assigneeString;
+                    }
+
+                    $actOnAssigneeUri = $true
                 }
-                if ($result)
+
+                if ($actOnIssueUri)
                 {
-                    Write-Debug "[Get-JiraIssue] Returned from invoking myself; outputting results"
-                    Write-Output $result
-                } else {
-                    Write-Debug "[Get-JiraIssue] Returned from invoking myself, but no results were found"
+                    Write-Debug "[Set-JiraIssue] IssueProps: [$issueProps]"
+
+                    Write-Debug "[Set-JiraIssue] Converting results to JSON"
+                    $json = ConvertTo-Json -InputObject $issueProps -Depth 5
+                    $issueObjURL = $issueObj.RestUrl
+
+                    Write-Debug "[Set-JiraIssue] Preparing for blastoff!"
+                    $issueResult = Invoke-JiraMethod -Method Put -URI $issueObjURL -Body $json -Credential $Credential
+                    Write-Debug "[Set-JiraIssue] Results are saved to issueResult variable"
                 }
+
+                if ($actOnAssigneeUri)
+                {
+                    # Jira handles assignee differently; you can't change it from the default "edit issues" screen unless
+                    # you customize the "Edit Issue" screen.
+
+                    $assigneeUrl = "{0}/assignee" -f $issueObj.RestUrl
+                    $json = ConvertTo-Json -InputObject $assigneeProps
+
+                    Write-Debug "[Set-JiraIssue] Preparing for blastoff!"
+                    $assigneeResult = Invoke-JiraMethod -Method Put -URI $assigneeUrl -Body $json -Credential $Credential
+                    Write-Debug "[Set-JiraIssue] Results are saved to assigneeResult variable"
+                }
+
+                if ($Label)
+                {
+                    Write-Debug "[Set-JiraIssue] Invoking Set-JiraIssueLabel to set issue labels"
+                    Set-JiraIssueLabel -Issue $issueObj -Set $Label -Credential $Credential
+                }
+
+                if ($PassThru)
+                {
+                    Write-Debug "[Set-JiraIssue] PassThru was specified. Obtaining updated reference to issue"
+                    Get-JiraIssue -Key $issueObj.Key -Credential $Credential
+                }
+
             } else {
-                Write-Debug "[Get-JiraIssue] Unable to identify filter [$Filter]"
-                Write-Error "Unable to identify filter [$Filter]. Check Get-JiraFilter for more details."
+                Write-Debug "[Set-JiraIssue] Unable to identify issue [$i]. Writing error message."
+                Write-Error "Unable to identify issue [$i]"
             }
         }
     }
 
     end
     {
-        Write-Debug "[Get-JiraIssue] Complete"
+        Write-Debug "[Set-JiraIssue] Complete"
     }
 }
+
+
