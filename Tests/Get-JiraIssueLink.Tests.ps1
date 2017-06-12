ef52391353c212f2ced97d2968c3dec211730dc7
Oliver Lipkau <oliver@lipkau.net>
2017-06-12 14:44:38 +0200

    implement Get-JiraIssueLink

---
+++ Tests/Get-JiraIssueLink.Tests.ps1
@@ -0,0 +1,78 @@
+. $PSScriptRoot\Shared.ps1
+
+InModuleScope PSJira {
+
+    [System.Diagnostics.CodeAnalysis.SuppressMessage('PSUseDeclaredVarsMoreThanAssigments', '', Scope='*', Target='SuppressImportModule')]
+    $SuppressImportModule = $true
+    . $PSScriptRoot\Shared.ps1
+
+    $jiraServer = 'http://jiraserver.example.com'
+
+    $issueLinkId = 1234
+
+    # We don't care about anything except for the id
+    $resultsJson = @"
+{
+    "id": "$issueLinkId",
+    "self": "",
+    "type": {},
+    "inwardIssue": {},
+    "outwardIssue": {}
+}
+"@
+
+    Describe "Get-JiraIssueLink" {
+        Mock Get-JiraConfigServer -ModuleName PSJira {
+            Write-Output $jiraServer
+        }
+
+        # Generic catch-all. This will throw an exception if we forgot to mock something.
+        Mock Invoke-JiraMethod -ModuleName PSJira {
+            ShowMockInfo 'Invoke-JiraMethod' 'Method', 'Uri'
+            throw "Unidentified call to Invoke-JiraMethod"
+        }
+
+        Mock Invoke-JiraMethod -ModuleName PSJira -ParameterFilter {$Method -eq 'Get' -and $URI -eq "$jiraServer/rest/api/2/issueLink/1234"} {
+            ConvertFrom-Json2 $resultsJson
+        }
+
+        Mock Get-JiraIssue -ModuleName PSJira -ParameterFilter {$Key -eq "TEST-01"} {
+            # We don't care about the content of any field except for the id
+            $obj = [PSCustomObject]@{
+                "id"          = $issueLinkId
+                "type"        = "foo"
+                "inwardIssue" = "bar"
+            }
+            $obj.PSObject.TypeNames.Insert(0, 'PSJira.IssueLink')
+            return [PSCustomObject]@{
+                issueLinks = @(
+                    $obj
+                )
+            }
+        }
+
+        #############
+        # Tests
+        #############
+
+        It "Returns details about specific issuelink" {
+            $result = Get-JiraIssueLink -Id $issueLinkId
+            $result | Should Not BeNullOrEmpty
+            @($result).Count | Should Be 1
+        }
+
+        It "Provides the key of the project" {
+            $result = Get-JiraIssueLink -Id $issueLinkId
+            $result.Id | Should Be $issueLinkId
+        }
+
+        It "Accepts input from pipeline" {
+            $result = (Get-JiraIssue -Key TEST-01).issuelinks | Get-JiraIssueLink
+            $result.Id | Should Be $issueLinkId
+        }
+
+        It 'Fails if input from the pipeline is of the wrong type' {
+            { [PSCustomObject]@{id = $issueLinkId} | Get-JiraIssueLink } | Should Throw
+        }
+    }
+}
