b793d93ccad48698e42c0ec057b0428d3b665488
Oliver Lipkau <oliver@lipkau.net>
2017-09-27 13:10:37 +0200

    Add a check that the header actually contains the key #185

--- JiraPS/Public/New-JiraSession.ps1
+++ JiraPS/Public/New-JiraSession.ps1
@@ -85,7 +85,10 @@ function New-JiraSession {
             # Test HEADERS if Jira requires a CAPTCHA
             $tokenRequiresCaptcha = "AUTHENTICATION_DENIED"
             $headerRequiresCaptcha = "X-Seraph-LoginReason"
-            if (($webResponse.Headers[$headerRequiresCaptcha] -split ",") -contains $tokenRequiresCaptcha) {
+            if (
+                $webResponse.Headers.ContainsKey($headerRequiresCaptcha) -and
+                ($webResponse.Headers[$headerRequiresCaptcha] -split ",") -contains $tokenRequiresCaptcha
+            ) {
                 Write-Warning "JIRA requires you to log on to the website before continuing for security reasons."
             }
 
