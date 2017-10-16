282dc72f4399885d7eb92a00646bf473a859b3d5
Oliver Lipkau <oliver@lipkau.net>
2017-10-16 09:08:57 +0200

    Use more generic creation of the POST payload

--- JiraPS/Public/Add-JiraIssueAttachment.ps1
+++ JiraPS/Public/Add-JiraIssueAttachment.ps1
@@ -11,7 +11,7 @@ function Add-JiraIssueAttachment {
        Get-JiraIssue "TEST-002" | Add-JiraIssueAttachment -FilePath "Test comment from PowerShell"
        This example illustrates pipeline use from Get-JiraIssue to Add-JiraIssueAttachment.
     .INPUTS
-       This function can accept PSJira.Issue objects via pipeline.
+       This function can accept JiraPS.Issue objects via pipeline.
     .OUTPUTS
        This function outputs the results of the attachment add.
     .NOTES
@@ -19,23 +19,24 @@ function Add-JiraIssueAttachment {
     #>
     [CmdletBinding()]
     param(
-        # Path of the file to upload and attach
+        # Issue to which to attach the file
         [Parameter(
             Position = 0,
             Mandatory = $true
         )]
-        [ValidateScript( { Test-Path $_ })]
-        [String] $FilePath,
+        [Alias('Key')]
+        [Object] $Issue,
 
-        # Issue to which to attach the file
+        # Path of the file to upload and attach
         [Parameter(
             Position = 1,
             Mandatory = $true,
             ValueFromPipeline = $true,
             ValueFromPipelineByPropertyName = $true
         )]
-        [Alias('Key')]
-        [Object] $Issue,
+        [ValidateScript( { Test-Path $_ })]
+        [Alias('InFile', 'FullName')]
+        [String[]] $FilePath,
 
         # Credentials to use to connect to Jira.
         [Parameter(Mandatory = $false)]
@@ -51,70 +52,72 @@ function Add-JiraIssueAttachment {
     }
 
     process {
-        # Validate input object
-        if (
-            # from Pipeline
-            (($_) -and ($_.PSObject.TypeNames[0] -ne "PSJira.Issue")) -or
-            # by parameter
-            ($Issue.PSObject.TypeNames[0] -ne "PSJira.Issue") -and (($Issue -isnot [String]))
-        ) {
-            $message = "Wrong object type provided for Issue. Was $($Issue.Gettype().Name)"
-            $exception = New-Object -TypeName System.ArgumentException -ArgumentList $message
-            Throw $exception
+        foreach ($file in $FilePath) {
+            # Validate input object
+            if (($Issue.PSObject.TypeNames[0] -ne "JiraPS.Issue") -and (($Issue -isnot [String]))) {
+                $message = "Wrong object type provided for Issue. Was $($Issue.GetType().Name)"
+                $exception = New-Object -TypeName System.ArgumentException -ArgumentList $message
+                Throw $exception
+            }
+
+            # As we are not able to use proper type casting in the parameters, this is a workaround
+            # to extract the data from a JiraPS.Issue object
+            Write-Debug "[Add-JiraIssueAttachment] Obtaining a reference to Jira issue [$Issue]"
+            if ($Issue.PSObject.TypeNames[0] -eq "JiraPS.Issue" -and $Issue.RestURL) {
+                $issueObj = $Issue
+            }
+            elseif ($Issue.PSObject.TypeNames[0] -eq "JiraPS.Issue" -and $Issue.Key) {
+                $issueObj = Get-JiraIssue -InputObject $Issue.Key -Credential $Credential -ErrorAction Stop
+            }
+            else {
+                $issueObj = Get-JiraIssue -InputObject $Issue -Credential $Credential -ErrorAction Stop
+            }
+
+            $url = "$($issueObj.RestURL)/attachments"
+
+            $fileName = Split-Path -Path $FilePath -Leaf
+            $readFile = [System.IO.File]::ReadAllBytes($FilePath)
+            $enc = [System.Text.Encoding]::GetEncoding("iso-8859-1")
+            $fileEnc = $enc.GetString($readFile)
+            $boundary = [System.Guid]::NewGuid().ToString()
+            $mimeType = [System.Web.MimeMapping]::GetMimeMapping($FilePath)
+            if ($mimeType) { $ContentType = $mimeType }
+            else { $ContentType = "application/octet-stream" }
+
+            $bodyLines = @'
+--{0}
+Content-Disposition: form-data; name="file"; filename="{1}"
+Content-Type: '{2}'
+
+{3}
+--{0}--
+
+'@ -f $boundary, $fileName, $mimeType, $fileEnc
+
+            $headers = @{
+                'X-Atlassian-Token' = 'nocheck'
+                'Content-Type'      = "multipart/form-data; boundary=`"$boundary`""
+            }
+            $parameter = @{
+                URI        = $url
+                Method     = "POST"
+                Body       = $bodyLines
+                Headers    = $headers
+                RawBody    = $true
+                Credential = $Credential
+            }
+            Write-Debug "[Add-JiraIssueAttachment] Preparing for blastoff!"
+            $rawResult = Invoke-JiraMethod @parameter
+
+            if ($PassThru) {
+                Write-Debug "[Add-JiraIssueAttachment] -PassThru specified."
+                Write-Debug "[Add-JiraIssueAttachment] Converting to custom object"
+                ConvertTo-JiraAttachment -InputObject $rawResult
+
+                Write-Debug "[Add-JiraIssueAttachment] Outputting result"
+                Write-Output $result
+            }
         }
-
-        # As we are not able to use proper type casting in the parameters, this is a workaround
-        # to extract the data from a PSJira.Issue object
-        Write-Debug "[Add-JiraIssueAttachment] Obtaining a reference to Jira issue [$Issue]"
-        if ($Issue.PSObject.TypeNames[0] -eq "PSJira.Issue" -and $Issue.RestURL) {
-            $issueObj = $Issue
-        }
-        else {
-            $issueObj = Get-JiraIssue -InputObject $Issue -Credential $Credential -ErrorAction Stop
-        }
-
-        $ID = $issueObj.ID
-        $url = "$($issueObj.RestURL)/attachments"
-
-        $LF = "`r`n"
-        $fileSize = (Get-Item $FilePath).Length
-        $fileName = (Split-Path -Path $FilePath -Leaf)
-        $readFile = [System.IO.File]::ReadAllBytes($FilePath)
-        $enc = [System.Text.Encoding]::GetEncoding("iso-8859-1")
-        $fileEnc = $enc.GetString($readFile)
-        $boundary = [System.Guid]::NewGuid().ToString()
-
-        $bodyLines =
-        "--$boundary$LF" +
-        "Content-Disposition: form-data; name=`"file`"; filename=`"$fileName`"; size=`"$fileSize`"; issueId=`"$ID`"$LF" +
-        "Content-Type: 'multipart/form-data'$LF$LF" +
-        "$fileEnc$LF" +
-        "--$boundary--$LF"
-
-        $headers = @{
-            'X-Atlassian-Token' = 'nocheck'
-            'Content-Type'      = "multipart/form-data; boundary=`"$boundary`""
-        }
-        $parameter = @{
-            URI        = $url
-            Method     = "POST"
-            Body       = $bodyLines
-            Headers    = $headers
-            RawBody    = $true
-            Credential = $Credential
-        }
-        Write-Debug "[Add-JiraIssueAttachment] Preparing for blastoff!"
-        $rawResult = Invoke-JiraMethod @parameter
-
-        if ($PassThru) {
-            Write-Debug "[Add-JiraIssueAttachment] -PassThru specified."
-            Write-Debug "[Add-JiraIssueAttachment] Converting to custom object"
-            ConvertTo-JiraAttachment -InputObject $rawResult
-
-            Write-Debug "[Add-JiraIssueAttachment] Outputting result"
-            Write-Output $result
-        }
-
     }
     end {
         Write-Debug "[Add-JiraIssueAttachment] Complete"
