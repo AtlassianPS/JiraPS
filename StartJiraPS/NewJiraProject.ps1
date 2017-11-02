b336a69b2097e7bbdd7b9c9dbe5cf08c336d5235
Michael Dejulia <mdejulia@LD.corp.local>
2017-11-02 16:47:52 -0700

    Startup Sandbox

---
+++ StartJiraPS/NewJiraProject.ps1
@@ -0,0 +1,1 @@
+New-jiraProject -Key MNP -Name 'My New Project' -Description 'My New Project Description' -Lead Mdejulia -PermissionScheme 0 -AssigneeType PROJECT_LEAD -IssueSecurityScheme 0 -NotificationScheme 0 -categoryId 0
---
+++ StartJiraPS/NewSession.ps1
@@ -0,0 +1,7 @@
+﻿Import-Module C:\git\JiraPS\JiraPS\JiraPS.psm1
+Set-JiraConfigServer -Server https://jira.loandepot.com
+If (-not $Creds) {
+    $Creds = Get-Credential mdejulia
+}
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+New-JiraSession -Credential $Creds -Verbose
