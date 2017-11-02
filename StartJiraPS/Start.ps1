c65872d6806ad820176ba8faa9767dd6987a0518
Michael Dejulia <mdejulia@LD.corp.local>
2017-11-02 16:47:52 -0700

    Startup Sandbox

---
+++ StartJiraPS/Start.ps1
@@ -0,0 +1,5 @@
+# New Session: Import-Module, Set-JiraServer, TLS, Credentials, New-JiraSession
+. C:\Git\StartJiraPS\NewSession.ps1
+
+# New Jira Project: New-JiraProject
+. C:\git\StartJiraPS\NewJiraProject.ps1
