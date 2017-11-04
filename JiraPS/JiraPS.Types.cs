fd8784041b24ee6cd95c3a1a9554dceada82c9c5
Oliver Lipkau <oliver@lipkau.net>
2017-11-04 12:05:35 +0100

    Add c# custom classes

---
+++ JiraPS/JiraPS.Types.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+
+namespace JiraPS {
+
+    public enum AssigneeType
+    {
+        PROJECT_LEAD,
+        UNASSIGNED
+    }
+
+}
