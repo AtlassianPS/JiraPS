ac4a95a2d3b07fbec27e71314be0f7431fec9034
Michael Dejulia <mdejulia@LD.corp.local>
2017-11-06 16:40:50 -0800

    Sandbox

--- StartJiraPS/NewJiraProject.ps1
+++ StartJiraPS/NewJiraProject.ps1
@@ -1,17 +1,19 @@
 
-$Params2 = {
-    key= "EX",
-    name= "Example",
-    projectTypeKey= "business",
-    projectTemplateKey= "com.atlassian.jira-core-project-templates:jira-core-project-management",
-    description= "Example Project description",
-    lead= "Michael.dejulia",
-    assigneeType= "ProjectLead",
-    avatarId= 10011,
-    issueSecurityScheme= 10000,
-    permissionScheme= 10000,
-    notificationScheme= 10000,
+$Params1 =  @{
+    key= "EX"
+    name= "Example"
+    projectTypeKey= "business"
+    projectTemplateKey= "com.atlassian.jira-core-project-templates:jira-core-project-management"
+    description= "Example Project description"
+    lead= "Michael.dejulia"
+    assigneeType= "ProjectLead"
+    URL = "https://google.net"
+    avatarId= 10011
+    issueSecurityScheme= 10000
+    permissionScheme= 10000
+    notificationScheme= 10000
     categoryId= 10000
 }
-
-New-jiraProject @Params2 -Verbose
+$Project = @()
+$Project += New-jiraProject @Params1 -Verbose
+#$project | Remove-JiraProject
