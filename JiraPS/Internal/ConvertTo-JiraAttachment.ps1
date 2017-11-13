38c86d1760183caeddbb9b6f9a72eca0d91f0723
Oliver Lipkau <oliver@lipkau.net>
2017-11-13 06:00:31 +0100

    Fix new ConvertTo-* functions

--- JiraPS/Internal/ConvertTo-JiraAttachment.ps1
+++ JiraPS/Internal/ConvertTo-JiraAttachment.ps1
@@ -1,51 +1,36 @@
-function ConvertTo-JiraAttachment
-{
+function ConvertTo-JiraAttachment {
     [CmdletBinding()]
     param(
-        [Parameter(Mandatory = $true,
-                   Position = 0,
-                   ValueFromPipeline = $true)]
+        [Parameter(
+            ValueFromPipeline = $true
+        )]
         [PSObject[]] $InputObject
     )
 
-    process
-    {
-        foreach ($i in $InputObject)
-        {
-#            Write-Debug "Processing object: '$i'"
+    process {
+        foreach ($i in $InputObject) {
+            Write-Debug "[$($MyInvocation.MyCommand.Name)] Converting `$InputObject to custom object"
 
-#            Write-Debug "Defining standard properties"
             $props = @{
-                'ID' = $i.id;
-                'self' = $i.self;
-                'fileName' = $i.FileName;
-                'author' = (ConvertTo-JiraUser -InputObject $i.Author);
-                'created' = Get-Date -Date ($i.created);
-                'size' = ([int]$i.size);
-                'mimeType' = $i.mimeType;
-                'content' = $i.content;
-                'thumbnail' = $i.thumbnail
+                'ID'        = $i.id
+                'Self'      = $i.self
+                'FileName'  = $i.FileName
+                'Author'    = ConvertTo-JiraUser -InputObject $i.Author
+                'Created'   = Get-Date -Date ($i.created)
+                'Size'      = ([Int]$i.size)
+                'MimeType'  = $i.mimeType
+                'Content'   = $i.content
+                'Thumbnail' = $i.thumbnail
             }
 
-#            Write-Debug "Creating PSObject out of properties"
             $result = New-Object -TypeName PSObject -Property $props
-
-#            Write-Debug "Inserting type name information"
             $result.PSObject.TypeNames.Insert(0, 'JiraPS.Attachment')
-
-#            Write-Debug "[ConvertTo-JiraProject] Inserting custom toString() method"
             $result | Add-Member -MemberType ScriptMethod -Name "ToString" -Force -Value {
-                Write-Output "$($this.fileName)"
+                Write-Output "$($this.FileName)"
             }
 
-#            Write-Debug "Outputting object"
             Write-Output $result
         }
-    }
-
-    end
-    {
-#        Write-Debug "Complete"
     }
 }
 
