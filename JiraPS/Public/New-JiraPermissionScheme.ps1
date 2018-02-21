220fcd4aa72f0485726cf9c443483706577634fa
Michael Dejulia <MDejulia@loandepot.com>
2018-02-21 08:53:41 -0800

    New-JiraPermissionSchema and Tests

---
+++ JiraPS/Public/New-JiraPermissionScheme.ps1
@@ -0,0 +1,65 @@
+function New-JiraPermissionScheme
+{
+    <#
+    .Synopsis
+
+    .DESCRIPTION
+       Create permission scheme
+    .EXAMPLE
+       New-JiraPermissionScheme
+    .EXAMPLE
+    .INPUTS
+    .OUTPUTS
+    #>
+    [CmdletBinding()]
+    param(
+        # Description of the permission scheme
+        [Parameter(Mandatory = $false)]
+        [String]
+        $Description,
+
+        # Name of the permission scheme
+        [Parameter(Mandatory = $true)]
+        [string]
+        $Name,
+
+        # JiraPS.PermissionScheme Object, Use Get-JiraPermissionScheme -ID $ID -Expand
+        [Parameter(Mandatory = $true)]
+        [Array]
+        $InputObject,
+
+        # Credentials to use to connect to JIRA.
+        # If not specified, this function will use anonymous access.
+        [Parameter(Mandatory = $false)]
+        [System.Management.Automation.PSCredential] $Credential
+    )
+
+    begin
+    {
+        $server = Get-JiraConfigServer -ConfigFile $ConfigFile -ErrorAction Stop
+        $restUri = "$server/rest/api/2/permissionscheme"
+    }
+
+    process
+    {
+        $props = @{
+            name        = $Name
+            description = $Description
+            permissions = $InputObject
+        }
+        $json = ConvertTo-Json -Depth 5 -InputObject $props
+        $results = Invoke-JiraMethod -Method POST -URI $restUri -Body $json -Credential $Credential
+        If ($results)
+        {
+            $results = ConvertTo-JiraPermissionScheme -InputObject $results
+        }
+        else
+        {
+            Write-Verbose "JIRA returned no results."
+        }
+    }
+    end
+    {
+        $results
+    }
+}
---
+++ Tests/New-JiraPermissionScheme.Tests.ps1
@@ -0,0 +1,72 @@
+. $PSScriptRoot\Shared.ps1
+
+InModuleScope JiraPS {
+
+    [System.Diagnostics.CodeAnalysis.SuppressMessage('PSUseDeclaredVarsMoreThanAssigments', '', Scope = '*', Target = 'SuppressImportModule')]
+    $SuppressImportModule = $true
+    . $PSScriptRoot\Shared.ps1
+
+    $jiraServer = 'http://jiraserver.example.com'
+
+    $description2 = 'PM_Permission_Scheme'
+    $name2 = 'PM'
+    $id2 = 17243
+
+    $inputObject = @"
+    {
+        "permissionSchemes":  [
+                                    {
+                                        "id":  "$ID2",
+                                        "name":  "$Name2",
+                                        "permissions":  [
+                                                            {
+                                                                "id":  "$ID2",
+                                                                "holder":  "@{type=group; parameter=jira-viewonly; group=; expand=group}",
+                                                                "permission":  "BROWSE_PROJECTS"
+                                                            },
+                                                            {
+                                                                "id":  "$ID2",
+                                                                "holder":  "@{type=group; parameter=jira-viewonly; group=; expand=group}",
+                                                                "permission":  "TRANSITION_ISSUES"
+                                                            }
+                                                        ]
+                                    }
+                                ]
+    }
+"@
+
+    $result = @"
+    {
+        "description":  "$description2",
+        "Name":  "$name2",
+        "ID":  "$id2"
+    }
+"@
+
+    Describe "New-JiraPermissionScheme" {
+
+        Mock Get-JiraConfigServer -ModuleName JiraPS {
+            Write-Output $jiraServer
+        }
+
+        Mock Invoke-JiraMethod -ModuleName JiraPS -ParameterFilter {$Method -eq 'POST' -and $URI -eq "$jiraServer/rest/api/2/permissionscheme"} {
+            $result | ConvertFrom-Json2
+        }
+
+        # Generic catch-all. This will throw an exception if we forgot to mock something.
+        Mock Invoke-JiraMethod -ModuleName JiraPS {
+            Write-Host "       Mocked Invoke-JiraMethod with no parameter filter." -ForegroundColor DarkRed
+            Write-Host "         [Method]         $Method" -ForegroundColor DarkRed
+            Write-Host "         [URI]            $URI" -ForegroundColor DarkRed
+            throw "Unidentified call to Invoke-JiraMethod"
+        }
+
+        It "New permission schema should return result" {
+            $newResult = New-JiraPermissionScheme -InputObject ($inputObject | ConvertFrom-Json2) -Name $name2 -Description $description2
+            $newResult | Should Not BeNullOrEmpty
+            $newResult.Name | Should Be $name2
+            $newResult.Description | Should Be $description2
+        }
+    }
+}
+
