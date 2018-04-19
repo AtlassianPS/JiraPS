a6bc0ecfe22df0f4b249fdc8e5c6cb8728125e0d
Oliver Lipkau <oliver@lipkau.net>
2018-04-19 09:49:57 +0200

    Updated Tools to include the installation of PowerShellGet on Powershell v3 and v4

--- Tools/setup.ps1
+++ Tools/setup.ps1
@@ -1,25 +1,52 @@
-# Make PSGallery trusted, to aviod a confirmation in the console
-Set-PSRepository -Name "PSGallery" -InstallationPolicy Trusted
+# PowerShell 5.1 and bellow need the PSGallery to be intialized
+if ($PSVersionTable.PSVersion.Major -le 5) {
+    if ($PSVersionTable.PSVersion.Major -in @(3, 4)) {
+        # If PowerShellGet is not available (PSv4 and PSv3), it must be installed
+        if (-not (Get-Module PowerShellGet -ListAvailable)) {
+            Write-Host "Installing PowershellGet"
+            Start-Process -FilePath "$env:SystemRoot\System32\msiexec.exe" -ArgumentList "/qb /i $(Join-Path $PSScriptRoot "PackageManagement_x64.msi")" -Wait
+        }
+    }
 
-Install-Module "InvokeBuild" -Scope CurrentUser -Force
+    if ("PSGallery" -notin (Get-PSRepository).Name) {
+        Write-Host "Installing PackageProvider NuGet"
+        # Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force | Out-Null
+        $null = Install-PackageProvider -Name NuGet -Force -ErrorAction Stop
+    }
+}
+
+# Make PSGallery trusted, to aviod a confirmation in the console
+Write-Host "Trusting PSGallery"
+Set-PSRepository -Name "PSGallery" -InstallationPolicy Trusted -ErrorAction Stop
+
+$ismoProp = @{
+    Scope       = "CurrentUser"
+    ErrorAction = "Stop"
+    Force       = $true
+    Verbose     = $true
+}
+
+Write-Host "Installing InvokeBuild"
+Install-Module "InvokeBuild" @ismoProp
 
 $buildHelpersConditions = @{}
 if ($PSVersionTable.PSVersion.Major -ge 5) {
     # PSv4 does not have the `-AllowClobber` parameter
     $buildHelpersConditions["AllowClobber"] = $true
 }
-Install-Module "BuildHelpers" -Scope CurrentUser @buildHelpersConditions
+Write-Host "Installing BuildHelpers"
+Install-Module "BuildHelpers" @ismoProp @buildHelpersConditions
 
-$pesterConditions = @{
-    RequiredVersion = "4.1.1"
-    Force           = $true
-}
+$pesterConditions = @{}
 if ($PSVersionTable.PSVersion.Major -ge 5) {
     # PSv4 does not have the `-SkipPublisherCheck` parameter
     $pesterConditions["SkipPublisherCheck"] = $true
 }
-Install-Module "Pester" -Scope CurrentUser @pesterConditions
+Write-Host "Installing Pester"
+Install-Module "Pester" @ismoProp -RequiredVersion "4.1.1" @pesterConditions
 
-Install-Module "platyPS" -Scope CurrentUser
+Write-Host "Installing platyPS"
+Install-Module "platyPS" @ismoProp -RequiredVersion "0.1.0.200"
 
-Install-Module "PSScriptAnalyzer" -Scope CurrentUser
+Write-Host "Installing PSScriptAnalyzer"
+Install-Module "PSScriptAnalyzer" @ismoProp
