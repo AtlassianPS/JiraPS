10ca2c7e7a44b39cc6651f57865e7ef672165f69
Oliver Lipkau <oliver@lipkau.net>
2018-04-23 22:27:12 +0200

    Added Get-JiraUser for the current user
    closes #231

--- Tests/Get-JiraUser.Tests.ps1
+++ Tests/Get-JiraUser.Tests.ps1
@@ -37,7 +37,7 @@ Describe "Get-JiraUser" {
     "displayName": "Powershell Test User",
     "active": true,
     "groups": {
-        "size": 5,
+        "size": 2,
         "items": [
             {
                 "name": "$testGroup1",
@@ -55,6 +55,16 @@ Describe "Get-JiraUser" {
 
         Mock Get-JiraConfigServer -ModuleName JiraPS {
             Write-Output $jiraServer
+        }
+
+        Mock ConvertTo-JiraUser -ModuleName JiraPS {
+            $InputObject
+        }
+
+        # Return information of the current user
+        Mock Invoke-JiraMethod -ModuleName JiraPS -ParameterFilter {$Method -eq 'Get' -and $URI -like "$jiraServer/rest/api/*/myself"} {
+            ShowMockInfo 'Invoke-JiraMethod' 'Method', 'Uri'
+            ConvertFrom-Json -InputObject $restResult
         }
 
         # Searching for a user.
@@ -79,34 +89,53 @@ Describe "Get-JiraUser" {
         # Tests
         #############
 
-        $getResult = Get-JiraUser -UserName $testUsername
+        It "Gets intormation about the loged in Jira user" {
+            $getResult = Get-JiraUser
+
+            $getResult | Should Not BeNullOrEmpty
+
+            Assert-MockCalled -CommandName Invoke-JiraMethod -Exactly 1 -Scope It -ParameterFilter {$URI -like "$jiraServer/rest/api/*/myself"}
+        }
 
         It "Gets information about a provided Jira user" {
+            $getResult = Get-JiraUser -UserName $testUsername
+
             $getResult | Should Not BeNullOrEmpty
+
+            Assert-MockCalled -CommandName Invoke-JiraMethod -Exactly 1 -Scope It -ParameterFilter {$URI -like "$jiraServer/rest/api/*/user?username=$testUsername&expand=groups"}
         }
 
         It "Returns all available properties about the returned user object" {
+            $getResult = Get-JiraUser -UserName $testUsername
+
             $restObj = ConvertFrom-Json -InputObject $restResult
 
-            $getResult.RestUrl | Should Be $restObj.self
+            $getResult.self | Should Be $restObj.self
             $getResult.Name | Should Be $restObj.name
             $getResult.DisplayName | Should Be $restObj.displayName
             $getResult.Active | Should Be $restObj.active
+
+            Assert-MockCalled -CommandName Invoke-JiraMethod -Exactly 1 -Scope It -ParameterFilter {$URI -like "$jiraServer/rest/api/*/user?username=$testUsername&expand=groups"}
         }
 
         It "Gets information for a provided Jira user if a JiraPS.User object is provided to the InputObject parameter" {
+            $getResult = Get-JiraUser -UserName $testUsername
             $result2 = Get-JiraUser -InputObject $getResult
+
             $result2 | Should Not BeNullOrEmpty
             $result2.Name | Should Be $testUsername
+
+            Assert-MockCalled -CommandName Invoke-JiraMethod -Exactly 2 -Scope It -ParameterFilter {$URI -like "$jiraServer/rest/api/*/user?username=$testUsername&expand=groups"}
         }
 
         It "Provides information about the user's group membership in Jira" {
-            $getResult.Groups | Should Not BeNullOrEmpty
-            $getResult.Groups[0] | Should Be $testGroup1
+            $getResult = Get-JiraUser -UserName $testUsername
+
+            $getResult.groups.size | Should Be 2
+            $getResult.groups.items[0].Name | Should Be $testGroup1
         }
 
         Context "Output checking" {
-            Mock ConvertTo-JiraUser {}
             Get-JiraUser -Username $testUsername | Out-Null
 
             It "Uses ConvertTo-JiraUser to beautify output" {
