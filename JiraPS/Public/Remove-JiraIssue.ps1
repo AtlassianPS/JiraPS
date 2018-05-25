ab6ed1e6fee21ad5ce043f9775e67d07d0b54490
Sean R. Williams <sean.r.williams@wsu.edu>
2018-05-25 14:56:55 -0700

    Initial implementation of Remove-JiraIssue w/ documentation

---
+++ JiraPS/Public/Remove-JiraIssue.ps1
@@ -0,0 +1,98 @@
+function Remove-JiraIssue {
+    [CmdletBinding(
+        ConfirmImpact = 'High',
+        SupportsShouldProcess
+    )]
+    param (
+        [Parameter( Mandatory, ValueFromPipeline )]
+        [ValidateNotNullOrEmpty()]
+        [ValidateScript(
+            {
+                if (("JiraPS.Issue" -notin $_.PSObject.TypeNames) -and (($_ -isnot [String]))) {
+                    $errorItem = [System.Management.Automation.ErrorRecord]::new(
+                        ([System.ArgumentException]"Invalid Type for Parameter"),
+                        'ParameterType.NotJiraIssue',
+                        [System.Management.Automation.ErrorCategory]::InvalidArgument,
+                        $_
+                    )
+                    $errorItem.ErrorDetails = "Wrong object type provided for Issue. Expected [JiraPS.Issue] or [String], but was $($_.GetType().Name)"
+                    $PSCmdlet.ThrowTerminatingError($errorItem)
+                    <#
+                      #ToDo:CustomClass
+                      Once we have custom classes, this check can be done with Type declaration
+                    #>
+                }
+                else {
+                    return $true
+                }
+            }
+        )]
+        [Object[]]
+        $Issue,
+
+        [Switch]
+        $IncludeSubTasks,
+
+        [PSCredential]
+        $Credential,
+
+        [Switch]
+        $Force
+    )
+
+    begin {
+        Write-Verbose "[$($MyInvocation.MyCommand.Name)] Function started"
+
+        $server = Get-JiraConfigServer -ErrorAction Stop
+
+        $resourceURi = "$server/rest/api/latest/issue/{0}?deleteSubtasks={1}"
+
+        if ($Force) {
+            Write-DebugMessage "[$($MyInvocation.MyCommand.Name)] -Force was passed. Backing up current ConfirmPreference [$ConfirmPreference] and setting to None"
+            $oldConfirmPreference = $ConfirmPreference
+            $ConfirmPreference = 'None'
+        }
+    }
+
+    process {
+
+        Write-DebugMessage "[$($MyInvocation.MyCommand.Name)] ParameterSetName: $($PsCmdlet.ParameterSetName)"
+        Write-DebugMessage "[$($MyInvocation.MyCommand.Name)] PSBoundParameters: $($PSBoundParameters | Out-String)"
+
+        foreach ($_issue in $Issue) {
+            Write-Verbose "[$($MyInvocation.MyCommand.Name)] Processing [$_issue]"
+            Write-Debug "[$($MyInvocation.MyCommand.Name)] Processing `$_issue [$_issue]"
+
+            $issueObj = Get-JiraIssue -InputObject $_issue -Credential $Credential -ErrorAction Stop
+
+            $parameter = @{
+                URI        = $resourceURi -f $issueObj.Key,$IncludeSubTasks
+                Method     = "DELETE"
+                Credential = $Credential
+            }
+
+            $TargetText = "[$($issueObj.Key)] $($issueObj.Summary)"
+            If ($IncludeSubTasks) {
+                $ActionText = "Remove issue and sub-tasks"
+            } Else {
+                $ActionText = "Remove issue"
+            }
+
+            if ($PSCmdlet.ShouldProcess($TargetText, $ActionText)) {
+
+                Write-Debug "[$($MyInvocation.MyCommand.Name)] Invoking JiraMethod with `$parameter"
+                Invoke-JiraMethod @parameter
+            }
+        }
+
+    }
+
+    end {
+        if ($Force) {
+            Write-DebugMessage "[$($MyInvocation.MyCommand.Name)] Restoring ConfirmPreference to [$oldConfirmPreference]"
+            $ConfirmPreference = $oldConfirmPreference
+        }
+
+        Write-Verbose "[$($MyInvocation.MyCommand.Name)] Complete"
+    }
+}
