c9cfd4384f80244e37e09227e8ad3e32cea87e3d
Oliver Lipkau <oliver@lipkau.net>
2018-06-25 18:31:17 +0200

    Added verbose messages and error for when body is empty

--- JiraPS/Private/Resolve-ErrorWebResponse.ps1
+++ JiraPS/Private/Resolve-ErrorWebResponse.ps1
@@ -11,6 +11,8 @@ function Resolve-ErrorWebResponse {
     )
 
     begin {
+        Write-Verbose "[$($MyInvocation.MyCommand.Name)] Function started"
+
         Write-DebugMessage "[$($MyInvocation.MyCommand.Name)] ParameterSetName: $($PsCmdlet.ParameterSetName)"
         Write-DebugMessage "[$($MyInvocation.MyCommand.Name)] PSBoundParameters: $($PSBoundParameters | Out-String)"
 
@@ -20,26 +22,27 @@ function Resolve-ErrorWebResponse {
         }
         # Powershell v5.1- has the body of the response in a Stream in the Exception Response
         else {
-            Write-Verbose 1
             $readStream = New-Object -TypeName System.IO.StreamReader -ArgumentList ($Exception.Exception.Response.GetResponseStream())
             $responseBody = $readStream.ReadToEnd()
             $readStream.Close()
         }
 
+        $exception = "Invalid Server Response"
+        $errorId = "InvalidResponse.Status$($StatusCode.value__)"
+        $errorCategory = "InvalidResult"
+
         if ($responseBody) {
             # Clear the body in case it is not a JSON (but rather html)
-            if ($responseBody -match "^[\s\t]*\<html\>") { $responseBody = '{"errorMessages": "Invalid server response. HTML returned."}' }
+            if ($responseBody -match "^[\s\t]*\<html\>") {
+                Write-DebugMessage "[$($MyInvocation.MyCommand.Name)] Content is HTML - replacing it with a generic json"
+                $responseBody = '{"errorMessages": "Invalid server response. HTML returned."}'
+            }
 
             Write-Verbose "[$($MyInvocation.MyCommand.Name)] Retrieved body of HTTP response for more information about the error (`$responseBody)"
             Write-Debug "[$($MyInvocation.MyCommand.Name)] Got the following error as `$responseBody"
 
-            $exception = "Invalid Server Response"
-            $errorId = "InvalidResponse.Status$($StatusCode.value__)"
-            $errorCategory = "InvalidResult"
-
             try {
                 $responseObject = ConvertFrom-Json -InputObject $responseBody -ErrorAction Stop
-                Write-Debug "aaaa `$responseObject"
 
                 foreach ($_error in ($responseObject.errorMessages + $responseObject.errors)) {
                     # $_error is a PSCustomObject - therefore can't be $false
@@ -57,6 +60,7 @@ function Resolve-ErrorWebResponse {
                 }
             }
             catch [ArgumentException] {
+                Write-DebugMessage "[$($MyInvocation.MyCommand.Name)] `$responseBody could not be converted from JSON"
                 $writeErrorSplat = @{
                     Exception    = $exception
                     ErrorId      = $errorId
@@ -79,5 +83,18 @@ function Resolve-ErrorWebResponse {
                 WriteError @writeErrorSplat
             }
         }
+        else {
+            Write-DebugMessage "[$($MyInvocation.MyCommand.Name)] Response had no Body. Using `$StatusCode for generic error"
+            $writeErrorSplat = @{
+                Exception    = $exception
+                ErrorId      = $errorId
+                Category     = $errorCategory
+                Message      = "Server responsed with $StatusCode"
+                Cmdlet       = $Cmdlet
+            }
+            WriteError @writeErrorSplat
+        }
+
+        Write-Verbose "[$($MyInvocation.MyCommand.Name)] Function ended"
     }
 }
