1e58170ffc2b94b76fbcc5234df33d9e193dd9de
Conor Tolan <ctolan@gmail.com>
2018-09-21 21:57:13 +0100

    Fixed issue with params from pipeline. Issue was due to begin and end blocks using params. Added test for params from pipeline.

--- Tests/New-JiraIssue.Tests.ps1
+++ Tests/New-JiraIssue.Tests.ps1
@@ -80,6 +80,8 @@
             'Description' = 'Test description';
         }
 
+        $pipelineParams = New-Object -TypeName PSCustomObject -Property $newParams
+
         Context "Sanity checking" {
             $command = Get-Command -Name New-JiraIssue
 
@@ -97,6 +99,13 @@
         Context "Behavior testing" {
             It "Creates an issue in JIRA" {
                 { New-JiraIssue @newParams } | Should Not Throw
+                # The String in the ParameterFilter is made from the keywords
+                # we should expect to see in the JSON that should be sent,
+                # including the summary provided in the test call above.
+                Assert-MockCalled -CommandName Invoke-JiraMethod -ModuleName JiraPS -Times 1 -Scope It -ParameterFilter { $Method -eq 'Post' -and $URI -like "$jiraServer/rest/api/*/issue" }
+            }
+            It "Creates an issue in JIRA from pipeline" {
+                { $pipelineParams | New-JiraIssue } | Should Not Throw
                 # The String in the ParameterFilter is made from the keywords
                 # we should expect to see in the JSON that should be sent,
                 # including the summary provided in the test call above.
