5a2204da311f19f17b0c4092c9dca7b71b23fb54
chattes5 <34773388+chattes5@users.noreply.github.com>
2022-09-12 18:15:39 +0530

    Update Get-JiraIssueCreateMetadata.ps1
    $resourceURi is updated due to the older API "https://jira.<ourdomain>/rest/api/2/issue/createmeta?projectKeys=<ProjectKey>&expand=projects.issuetypes.fields" is inactiv/removed. The new URI is "/rest/api/2/issue/createmeta/{projectIdOrKey}/issuetypes"

--- JiraPS/Public/Get-JiraIssueCreateMetadata.ps1
+++ JiraPS/Public/Get-JiraIssueCreateMetadata.ps1
@@ -21,7 +21,7 @@ function Get-JiraIssueCreateMetadata {
 
         $server = Get-JiraConfigServer -ErrorAction Stop
 
-        $resourceURi = "$server/rest/api/2/issue/createmeta?projectIds={0}&issuetypeIds={1}&expand=projects.issuetypes.fields"
+        $resourceURi = "$server/rest/api/2/issue/createmeta/{0}/issuetypes"
     }
 
     process {
