824eb043b6dc42caea0002ad4e8c409586dede36
hmmwhatsthisdo <2093321+hmmwhatsthisdo@users.noreply.github.com>
2024-08-01 19:27:08 -0700

    Refactor to unify v8/v9+ CreateMeta response rehydration

--- JiraPS/Private/ConvertTo-JiraCreateMetaField.ps1
+++ JiraPS/Private/ConvertTo-JiraCreateMetaField.ps1
@@ -14,95 +14,63 @@ function ConvertTo-JiraCreateMetaField {
             $fieldList = $null
 
             If ($null -ne $i.values) {
-                $fieldList = $i.values
+                $fieldList = $i.values | ForEach-Object {
+                    @{
+                        Name = $_.fieldId
+                        Value = $_
+                    }
+                }
             }
             Elseif ($null -ne $i.projects.issuetypes.fields) {
 
+                $fields = $i.projects.issuetypes.fields
+                $fieldNames = (Get-Member -InputObject $fields -MemberType '*Property').Name
+
+                $fieldList = $fieldNames | ForEach-Object {
+                    @{
+                        Name = $_
+                        Value = $fields.$_
+                    }
+                }
             }
             Else {
                 Write-Error -Exception ([System.ArgumentException]::new("Input data does not match a known CreateMeta payload.", "InputObject"))
             }
 
-            if ($null -ne $i.values) {
+            $FieldList | ForEach-Object {
+                $item = $_.Value
 
-                $i.values | ForEach-Object {
-                    $item = $_
-
-                    $props = @{
-                        'Id'              = $item.fieldId
-                        'Name'            = $item.name
-                        'HasDefaultValue' = [System.Convert]::ToBoolean($item.hasDefaultValue)
-                        'Required'        = [System.Convert]::ToBoolean($item.required)
-                        'Schema'          = $item.schema
-                        'Operations'      = $item.operations
-                    }
-
-                    if ($item.allowedValues) {
-                        $props.AllowedValues = $item.allowedValues
-                    }
-
-                    if ($item.autoCompleteUrl) {
-                        $props.AutoCompleteUrl = $item.autoCompleteUrl
-                    }
-
-                    foreach ($extraProperty in (Get-Member -InputObject $item -MemberType NoteProperty).Name) {
-                        if ($null -eq $props.$extraProperty) {
-                            $props.$extraProperty = $item.$extraProperty
-                        }
-                    }
-
-                    $result = New-Object -TypeName PSObject -Property $props
-                    $result.PSObject.TypeNames.Insert(0, 'JiraPS.CreateMetaField')
-                    $result | Add-Member -MemberType ScriptMethod -Name "ToString" -Force -Value {
-                        Write-Output "$($this.Name)"
-                    }
-
-                    Write-Output $result
+                $props = @{
+                    'Id'              = $_.Name
+                    'Name'            = $item.name
+                    'HasDefaultValue' = [System.Convert]::ToBoolean($item.hasDefaultValue)
+                    'Required'        = [System.Convert]::ToBoolean($item.required)
+                    'Schema'          = $item.schema
+                    'Operations'      = $item.operations
                 }
 
-            }
-            else {
-
-                $fields = $i.projects.issuetypes.fields
-                $fieldNames = (Get-Member -InputObject $fields -MemberType '*Property').Name
-                foreach ($f in $fieldNames) {
-
-
-                    $item = $fields.$f
-
-                    $props = @{
-                        'Id'              = $f
-                        'Name'            = $item.name
-                        'HasDefaultValue' = [System.Convert]::ToBoolean($item.hasDefaultValue)
-                        'Required'        = [System.Convert]::ToBoolean($item.required)
-                        'Schema'          = $item.schema
-                        'Operations'      = $item.operations
-                    }
-
-                    if ($item.allowedValues) {
-                        $props.AllowedValues = $item.allowedValues
-                    }
-
-                    if ($item.autoCompleteUrl) {
-                        $props.AutoCompleteUrl = $item.autoCompleteUrl
-                    }
-
-                    foreach ($extraProperty in (Get-Member -InputObject $item -MemberType NoteProperty).Name) {
-                        if ($null -eq $props.$extraProperty) {
-                            $props.$extraProperty = $item.$extraProperty
-                        }
-                    }
-
-                    $result = New-Object -TypeName PSObject -Property $props
-                    $result.PSObject.TypeNames.Insert(0, 'JiraPS.CreateMetaField')
-                    $result | Add-Member -MemberType ScriptMethod -Name "ToString" -Force -Value {
-                        Write-Output "$($this.Name)"
-                    }
-
-                    Write-Output $result
+                if ($item.allowedValues) {
+                    $props.AllowedValues = $item.allowedValues
                 }
-            }
 
+                if ($item.autoCompleteUrl) {
+                    $props.AutoCompleteUrl = $item.autoCompleteUrl
+                }
+
+                foreach ($extraProperty in (Get-Member -InputObject $item -MemberType NoteProperty).Name) {
+                    if ($null -eq $props.$extraProperty) {
+                        $props.$extraProperty = $item.$extraProperty
+                    }
+                }
+
+                $result = New-Object -TypeName PSObject -Property $props
+                $result.PSObject.TypeNames.Insert(0, 'JiraPS.CreateMetaField')
+                $result | Add-Member -MemberType ScriptMethod -Name "ToString" -Force -Value {
+                    Write-Output "$($this.Name)"
+                }
+
+                Write-Output $result
+            }
         }
     }
 }
