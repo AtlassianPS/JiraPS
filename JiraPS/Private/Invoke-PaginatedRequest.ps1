61840f85983dc5afa55f2de01d2017cd3015a71f
Jonatan Rek <gamerclassn7@gmail.com>
2025-09-09 17:48:22 +0200

    Fixes

--- JiraPS/Private/Invoke-PaginatedRequest.ps1
+++ JiraPS/Private/Invoke-PaginatedRequest.ps1
@@ -1,4 +1,5 @@
 function Invoke-PaginatedRequest {
+    [CmdletBinding(SupportsPaging = $true)]
     param(
         [Parameter( Mandatory )]
         [Uri]
@@ -46,14 +47,14 @@ function Invoke-PaginatedRequest {
         [System.Management.Automation.Credential()]
         $Credential = [System.Management.Automation.PSCredential]::Empty,
 
+        [Parameter( Mandatory = $true )]
+        [PSCustomObject]
+        $Response,
+
         # [Parameter( DontShow )]
         [ValidateNotNullOrEmpty()]
         [System.Management.Automation.PSCmdlet]
-        $Cmdlet = $PSCmdlet,
-
-        [Parameter( Mandatory = $true )]
-        [PSCustomObject]
-        $response
+        $Cmdlet = $PSCmdlet
     )
 
     process {
@@ -61,6 +62,8 @@ function Invoke-PaginatedRequest {
         #$script:PSDefaultParameterValues.Remove("$($MyInvocation.MyCommand.Name):IncludeTotalCount")
         $null = $PSBoundParameters.Remove("Paging")
         $null = $PSBoundParameters.Remove("Skip")
+        $null = $PSBoundParameters.Remove("Response")
+
         if (-not $PSBoundParameters["GetParameter"]) {
             $PSBoundParameters["GetParameter"] = $internalGetParameter
         }
@@ -69,7 +72,7 @@ function Invoke-PaginatedRequest {
         $onceMore = $true
         do {
             Write-Verbose "[$($MyInvocation.MyCommand.Name)] Invoking pagination [currentTotal: $total]"
-
+write-host $total
             $result = Expand-Result -InputObject $response # NOTE: $response still need to be added as input to this function
 
             $total += @($result).Count
@@ -78,7 +81,6 @@ function Invoke-PaginatedRequest {
                 $pageSize = $response.maxResults
             }
 
-            write-host ($PSCmdlet.PagingParameters | ConvertTo-Json)
             if ($total -gt $PSCmdlet.PagingParameters.First) {
                 Write-DebugMessage "[$($MyInvocation.MyCommand.Name)] Only output the first $($PSCmdlet.PagingParameters.First % $pageSize) of page"
                 $result = $result | Select-Object -First ($PSCmdlet.PagingParameters.First % $pageSize)
@@ -100,11 +102,12 @@ function Invoke-PaginatedRequest {
             switch -regex ($PSBoundParameters["Uri"]) {
                 '\/rest\/api\/3' {
                     # v3 of the API implemented pagination by token
-                    if ($null -ne $response.Contains("nextPageToken")) {
+                    if ($response.PSObject.properties.name.Contains("nextPageToken")) {
                         $PSBoundParameters["GetParameter"]["nextPageToken"] = $response.nextPageToken
                     }
                     else {
                         # Fix Last page
+                        Write-DebugMessage "Last Page"
                         $onceMore = $false # NOTE: I would like to remove this and have the same behavior as v2: it's fine if we query 1 empty page at the end
                     }
                 }
@@ -121,8 +124,6 @@ function Invoke-PaginatedRequest {
 
             # Inquire the next page
             $response = Invoke-JiraMethod @PSBoundParameters
-            Expand-Result -InputObject $response # NOTE: I would like to write to the pipeline here without capturing
-
         } while (($response.isLast -eq $false) -or $onceMore ) # keep the previous logic: fetch one more page and repeat if not empty -- or: optimize by comparing results on page to pagesize
 
         if ($PSCmdlet.PagingParameters.IncludeTotalCount) {
