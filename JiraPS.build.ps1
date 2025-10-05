de95de50bf4e8e9c2a0d1f04a4ae1e4fc89a783d
Steven Bailey <sraybailey@gmail.com>
2025-10-05 17:35:11 -0400

    Updated Pester config setup

--- JiraPS.build.ps1
+++ JiraPS.build.ps1
@@ -220,15 +220,28 @@ task Test Init, {
     $codeCoverageFiles = Get-ChildItem @params #>
 
     try {
+        $testConfig = New-PesterConfiguration -Hashtable @{
+            Run = @{
+                PassThru = $true
+                Path = "$env:BHBuildOutput/Tests/*"
+            }
+            TestResult = @{
+                OutputFormat = 'NUnitXml'
+                OutputPath = "$env:BHProjectPath/Test-$OS-$($PSVersionTable.PSVersion.ToString()).xml"
+            }
+            Output = @{
+                Verbosity = 'Detailed'
+            }
+            Filter = @{
+                Tag        = $Tag
+                ExcludeTag = $ExcludeTag
+            }
+            <# CodeCoverage = @{
+                Path = $codeCoverageFiles
+            } #>
+        }
         $parameter = @{
-            Script       = "$env:BHBuildOutput/Tests/*"
-            Tag          = $Tag
-            ExcludeTag   = $ExcludeTag
-            Show         = "Fails"
-            PassThru     = $true
-            OutputFile   = "$env:BHProjectPath/Test-$OS-$($PSVersionTable.PSVersion.ToString()).xml"
-            OutputFormat = "NUnitXml"
-            # CodeCoverage = $codeCoverageFiles
+            Configuration = $testConfig
         }
         $testResults = Invoke-Pester @parameter
 
