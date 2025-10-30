b717708eaf87e65d228fe92a33861b6abeffc849
Steven Bailey <sraybailey@gmail.com>
2025-10-29 20:29:56 -0400

    Updated all Add-* function tests

--- Tests/Functions/Add-JiraIssueComment.Unit.Tests.ps1
+++ Tests/Functions/Add-JiraIssueComment.Unit.Tests.ps1
@@ -4,7 +4,7 @@
 Describe "Add-JiraIssueComment" -Tag 'Unit' {
 
     BeforeAll {
-        Remove-Item -Path Env:\BH*
+        Remove-Item -Path Env:\BH* -ErrorAction SilentlyContinue
         $projectRoot = (Resolve-Path "$PSScriptRoot/../..").Path
         if ($projectRoot -like "*Release") {
             $projectRoot = (Resolve-Path "$projectRoot/..").Path
@@ -22,19 +22,12 @@ Describe "Add-JiraIssueComment" -Tag 'Unit' {
             $env:BHManifestToTest = $env:BHBuildModuleManifest
         }
 
-        Import-Module "$env:BHProjectPath/Tools/BuildTools.psm1"
+        Import-Module "$env:BHProjectPath/Tools/BuildTools.psm1" -ErrorAction Stop
 
         Remove-Module $env:BHProjectName -ErrorAction SilentlyContinue
-        Import-Module $env:BHManifestToTest
-    }
-    AfterAll {
-        Remove-Module $env:BHProjectName -ErrorAction SilentlyContinue
-        Remove-Module BuildHelpers -ErrorAction SilentlyContinue
-        Remove-Item -Path Env:\BH*
-    }
+        Import-Module $env:BHManifestToTest -ErrorAction Stop
 
-    InModuleScope JiraPS {
-
+        # helpers used by tests (defParam / ShowMockInfo)
         . "$PSScriptRoot/../Shared.ps1"
 
         $jiraServer = 'http://jiraserver.example.com'
@@ -51,65 +44,124 @@ Describe "Add-JiraIssueComment" -Tag 'Unit' {
 }
 "@
 
-        Mock Get-JiraConfigServer {
-            Write-Output $jiraServer
-        }
-
-        Mock Get-JiraIssue {
+        # Helper function for creating issue objects
+        function New-TestJiraIssue {
+            param($Key = $issueKey)
             $object = [PSCustomObject] @{
                 ID      = $issueID
-                Key     = $issueKey
+                Key     = $Key
                 RestUrl = "$jiraServer/rest/api/2/issue/$issueID"
             }
             $object.PSObject.TypeNames.Insert(0, 'JiraPS.Issue')
             return $object
         }
 
-        Mock Resolve-JiraIssueObject -ModuleName JiraPS {
-            Get-JiraIssue -Key $Issue
+        Mock Get-JiraConfigServer -ModuleName JiraPS {
+            Write-Output $jiraServer
         }
 
-        Mock Invoke-JiraMethod -ParameterFilter {$Method -eq 'POST' -and $URI -eq "$jiraServer/rest/api/2/issue/$issueID/comment"} {
+        Mock Get-JiraIssue -ModuleName JiraPS {
+            New-TestJiraIssue -Key $Key
+        }
+
+        Mock Invoke-JiraMethod -ModuleName JiraPS -ParameterFilter {$Method -eq 'POST' -and $URI -eq "$jiraServer/rest/api/2/issue/$issueID/comment"} {
             ShowMockInfo 'Invoke-JiraMethod' 'Method', 'Uri'
             ConvertFrom-Json $restResponse
         }
 
         # Generic catch-all. This will throw an exception if we forgot to mock something.
-        Mock Invoke-JiraMethod {
+        Mock Invoke-JiraMethod -ModuleName JiraPS {
             ShowMockInfo 'Invoke-JiraMethod' 'Method', 'Uri'
             throw "Unidentified call to Invoke-JiraMethod"
         }
+    }
 
-        #############
-        # Tests
-        #############
+    #############
+    # Tests
+    #############
 
-        It "Adds a comment to an issue in JIRA" {
-            $commentResult = Add-JiraIssueComment -Comment 'This is a test comment from Pester.' -Issue $issueKey
-            $commentResult | Should -Not -BeNullOrEmpty
+    Context "Sanity checking" {
+        It "Has expected parameters" {
+            $command = Get-Command -Name Add-JiraIssueComment
 
-            Assert-MockCalled 'Get-JiraIssue' -ModuleName JiraPS -Exactly -Times 1 -Scope It
-            Assert-MockCalled 'Resolve-JiraIssueObject' -ModuleName JiraPS -Exactly -Times 1 -Scope It
-            Assert-MockCalled 'Invoke-JiraMethod' -ModuleName JiraPS -Exactly -Times 1 -Scope It
+            defParam $command 'Comment'
+            defParam $command 'Issue'
+            defParam $command 'VisibleRole'
+            defParam $command 'Credential'
         }
+    }
 
-        It "Accepts pipeline input from Get-JiraIssue" {
-            $commentResult = Get-JiraIssue -Key $IssueKey | Add-JiraIssueComment -Comment 'This is a test comment from Pester, using the pipeline!'
-            $commentResult | Should -Not -BeNullOrEmpty
+    Context "Behavior testing" {
+        Context "Intended Processing" {
+            It "Adds a comment to an issue in JIRA" {
+                $commentResult = Add-JiraIssueComment -Comment 'This is a test comment from Pester.' -Issue $issueKey
+                $commentResult | Should -Not -BeNullOrEmpty
+            }
 
-            Assert-MockCalled 'Get-JiraIssue' -ModuleName JiraPS -Exactly -Times 2 -Scope It
-            Assert-MockCalled 'Resolve-JiraIssueObject' -ModuleName JiraPS -Exactly -Times 1 -Scope It
-            Assert-MockCalled 'Invoke-JiraMethod' -ModuleName JiraPS -Exactly -Times 1 -Scope It
+            It "Accepts pipeline input from Get-JiraIssue" {
+                # Mock for when Get-JiraIssue is called directly in tests (outside module scope)
+                Mock Invoke-JiraMethod -ModuleName JiraPS -ParameterFilter {$Method -eq 'GET' -and $URI -like "$jiraServer/rest/api/2/issue/$issueKey*"} {
+                    ShowMockInfo 'Invoke-JiraMethod' 'Method', 'Uri'
+                    @{
+                        id = $issueID
+                        key = $issueKey
+                        self = "$jiraServer/rest/api/2/issue/$issueID"
+                        fields = @{}
+                    }
+                }
+
+                $commentResult = Get-JiraIssue -Key $IssueKey |
+                    Add-JiraIssueComment -Comment 'This is a test comment from Pester, using the pipeline!'
+                $commentResult | Should -Not -BeNullOrEmpty
+            }
         }
 
         Context "Output checking" {
-            Mock ConvertTo-JiraComment {}
-            Add-JiraIssueComment -Comment 'This is a test comment from Pester.' -Issue $issueKey | Out-Null
-
+            BeforeAll {
+                Mock ConvertTo-JiraComment -ModuleName JiraPS {
+                    $InputObject
+                }
+            }
 
             It "Uses ConvertTo-JiraComment to beautify output" {
-                Assert-MockCalled 'ConvertTo-JiraComment'
+                Add-JiraIssueComment -Comment 'This is a test comment from Pester.' -Issue $issueKey | Out-Null
+
+                Should -Invoke 'ConvertTo-JiraComment' -ModuleName JiraPS -Exactly -Times 1
             }
         }
+
+        Context "Internal Call Validation" {
+            It "Executes actual API call using Invoke-JiraMethod" {
+                Add-JiraIssueComment -Comment 'This is a test comment from Pester.' -Issue $issueKey | Out-Null
+
+                Should -Invoke 'Invoke-JiraMethod' -ModuleName JiraPS -Exactly -Times 1
+            }
+
+            It "Uses ConvertTo-JiraComment to beutify output" {
+                Mock ConvertTo-JiraComment -ModuleName JiraPS {
+                    $InputObject
+                }
+
+                Add-JiraIssueComment -Comment 'This is a test comment from Pester.' -Issue $issueKey | Out-Null
+
+                Should -Invoke 'ConvertTo-JiraComment' -ModuleName JiraPS -Exactly -Times 1
+            }
+
+            It "Calls Resolve-JiraIssueObject to set the issue object" {
+                Mock Resolve-JiraIssueObject -ModuleName JiraPS {
+                    New-TestJiraIssue -Key $issueKey
+                }
+
+                Add-JiraIssueComment -Comment 'This is a test comment from Pester.' -Issue $issueKey | Out-Null
+
+                Should -Invoke 'Resolve-JiraIssueObject' -ModuleName JiraPS -Exactly -Times 1
+            }
+        }
+    }
+
+    AfterAll {
+        Remove-Module $env:BHProjectName -ErrorAction SilentlyContinue
+        Remove-Module BuildHelpers -ErrorAction SilentlyContinue
+        Remove-Item -Path Env:\BH* -ErrorAction SilentlyContinue
     }
 }
